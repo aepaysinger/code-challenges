7d16daf1186de88402973e328ae3d2ced754ab49
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-01 13:11:19 -0500

    fixed the names of functions

--- tests/hacker_rank/test_if_else.py
+++ tests/hacker_rank/test_if_else.py
@@ -4,7 +4,7 @@ import sys
 from code_challenges.hacker_rank.if_else import weird_stuff
 
 
-def test_weird_stuff_not_weird():
+def test_weird_stuff_not_weird_a():
     capturedOutput = io.StringIO()                  # Create StringIO object
     sys.stdout = capturedOutput                     #  and redirect stdout.
     weird_stuff(4)                                  # Call function.
@@ -13,7 +13,7 @@ def test_weird_stuff_not_weird():
     assert capturedOutput.getvalue() == "Not Weird\n", f"Printed: {capturedOutput.getvalue()}, instead of Not Weird"
 # not the \n in the string
 
-def test_weird_stuff_weird():
+def test_weird_stuff_weird_a():
     capturedOutput = io.StringIO()                  # Create StringIO object
     sys.stdout = capturedOutput                     #  and redirect stdout.
     weird_stuff(3)                                  # Call function.
@@ -21,7 +21,7 @@ def test_weird_stuff_weird():
 
     assert capturedOutput.getvalue() == "Weird\n", f"Printed: {capturedOutput.getvalue()}, instead of Weird"
 
-def test_weird_stuff_weird():
+def test_weird_stuff_weird_b():
     capturedOutput = io.StringIO()                  # Create StringIO object
     sys.stdout = capturedOutput                     #  and redirect stdout.
     weird_stuff(8)                                  # Call function.
@@ -29,7 +29,7 @@ def test_weird_stuff_weird():
 
     assert capturedOutput.getvalue() == "Weird\n", f"Printed: {capturedOutput.getvalue()}, instead of Weird"
 
-def test_weird_stuff_weird():
+def test_weird_stuff_not_weird_b():
     capturedOutput = io.StringIO()                  # Create StringIO object
     sys.stdout = capturedOutput                     #  and redirect stdout.
     weird_stuff(22)                                  # Call function.
