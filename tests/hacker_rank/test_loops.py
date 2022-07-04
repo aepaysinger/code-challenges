00837c5c604c2cd4f14dca17f12c67e35f6e70d9
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-04 05:40:55 -0500

    still working on loops

--- tests/hacker_rank/test_loops.py
+++ tests/hacker_rank/test_loops.py
@@ -3,10 +3,12 @@ import sys
 
 from code_challenges.hacker_rank.loops import fun_stuff
 
+
+
 def test_fun_stuff():
     capturedOutput = io.StringIO()                  # Create StringIO object
     sys.stdout = capturedOutput                     #  and redirect stdout.
     fun_stuff(4)                                  # Call function.
     sys.stdout = sys.__stdout__                     # Reset redirect.
 
-    assert capturedOutput.getvalue() == [print(i*i) for i in range(4)], f"Printed: {capturedOutput.getvalue()}, instead of {[print(i*i) for i in range(4)]}"
+    assert capturedOutput.getvalue() == "".join([f"{i*i}\n" for i in range(4)]), f"Printed: {capturedOutput.getvalue()}, instead of {"".join(["(i*i)\n" for i in range(4)])}"
