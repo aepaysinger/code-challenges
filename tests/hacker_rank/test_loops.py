c56ea213bdff52822ed874d872c7a1ca60e90a27
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-06 06:20:20 -0500

    finishing and cleaning up loops

--- tests/hacker_rank/test_loops.py
+++ tests/hacker_rank/test_loops.py
@@ -4,11 +4,35 @@ import sys
 from code_challenges.hacker_rank.loops import fun_stuff
 
 
+def test_fun_stuff_a():
+    reps = 4
+    expected = "".join([f"{i*i}\n" for i in range(reps)])
+    capturedOutput = io.StringIO()                  
+    sys.stdout = capturedOutput                     
+    fun_stuff(reps)                                  
+    sys.stdout = sys.__stdout__                     
 
-def test_fun_stuff():
-    capturedOutput = io.StringIO()                  # Create StringIO object
-    sys.stdout = capturedOutput                     #  and redirect stdout.
-    fun_stuff(4)                                  # Call function.
-    sys.stdout = sys.__stdout__                     # Reset redirect.
+    assert capturedOutput.getvalue() == expected, f"Printed: {capturedOutput.getvalue()}, instead of {expected}"
 
-    assert capturedOutput.getvalue() == "".join([f"{i*i}\n" for i in range(4)]), f"Printed: {capturedOutput.getvalue()}, instead of {"".join(["(i*i)\n" for i in range(4)])}"
+
+def test_fun_stuff_b():
+    reps = -3
+    expected = "".join([f"{i*i}\n" for i in range(reps)])
+    capturedOutput = io.StringIO()                  
+    sys.stdout = capturedOutput                     
+    fun_stuff(reps)                                  
+    sys.stdout = sys.__stdout__                     
+
+    assert capturedOutput.getvalue() == expected, f"Printed: {capturedOutput.getvalue()}, instead of {expected}"
+
+
+def test_fun_stuff_c():
+    reps = 7
+    expected = "".join([f"{i*i}\n" for i in range(reps)])
+    capturedOutput = io.StringIO()                  
+    sys.stdout = capturedOutput                     
+    fun_stuff(reps)                                  
+    sys.stdout = sys.__stdout__                     
+
+    assert capturedOutput.getvalue() == expected, f"Printed: {capturedOutput.getvalue()}, instead of {expected}"
+
