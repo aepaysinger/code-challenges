d0db5761d530ccfbfb24f444cba97d605ed36d1a
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-06 18:21:12 -0500

    still workin gon set difference operation

--- tests/hacker_rank/test_set_difference_operation.py
+++ tests/hacker_rank/test_set_difference_operation.py
@@ -4,10 +4,28 @@ import sys
 from code_challenges.hacker_rank.set_difference_operation import learning_difference
 
 
-def test_weird_stuff_not_weird_a():
-    capturedOutput = io.StringIO()                  # Create StringIO object
-    sys.stdout = capturedOutput                     #  and redirect stdout.
-    weird_stuff(4)                                  # Call function.
-    sys.stdout = sys.__stdout__                     # Reset redirect.
+def test_learning_difference_result_4():
+    capturedOutput = io.StringIO()                 
+    sys.stdout = capturedOutput                    
+    learning_difference(['1', '2', '3', '4', '5', '6', '7', '8', '9'],['10', '1', '2', '3', '11', '21', '55', '6', '8'])                                  
+    sys.stdout = sys.__stdout__                     
 
-    assert capturedOutput.getvalue() == "Not Weird\n", f"Printed: {capturedOutput.getvalue()}, instead of Not Weird"
+    assert capturedOutput.getvalue() == '4\n', f"Printed: {capturedOutput.getvalue()}, instead of 4"
+
+
+def test_learning_difference_result_4():
+    capturedOutput = io.StringIO()                 
+    sys.stdout = capturedOutput                    
+    learning_difference(['9', '2', '10', '4', '8', '10', '7', '45', '9'],['7', '100', '2', '3', '11', '8', '8', '6', '8'])                                  
+    sys.stdout = sys.__stdout__                     
+
+    assert capturedOutput.getvalue() == '20\n', f"Printed: {capturedOutput.getvalue()}, instead of 4"
+
+
+def test_learning_difference_result_4():
+    capturedOutput = io.StringIO()                 
+    sys.stdout = capturedOutput                    
+    learning_difference(['1', '2', '3', '4', '5', '6', '7', '8', '9'],['10', '1', '2', '3', '11', '21', '55', '6', '8'])                                  
+    sys.stdout = sys.__stdout__                     
+
+    assert capturedOutput.getvalue() == '4\n', f"Printed: {capturedOutput.getvalue()}, instead of 4"
