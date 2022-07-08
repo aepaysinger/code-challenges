70610a9cc6e331c6f50dbad905693c26abb6d2dd
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-08 10:31:27 -0500

    adding print function

---
+++ code_challenges/hacker_rank/print_function.py
@@ -0,0 +1,9 @@
+def count_up(n):
+    result = ""
+    for number in range(n):
+        result = result + str(number + 1)
+    return result
+
+if __name__ == '__main__':
+    n = int(input())
+    print(count_up(n))
