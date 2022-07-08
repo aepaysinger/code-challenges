2c92b4266e36dcee76cef9bc220419c7a0de16dd
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-08 13:19:08 -0500

    cleaning up intergers come in all sizes

--- tests/hacker_rank/test_intergers_come_in_all_sizes.py
+++ tests/hacker_rank/test_intergers_come_in_all_sizes.py
@@ -5,27 +5,27 @@ from code_challenges.hacker_rank.intergers_come_in_all_sizes import big_interger
 
 
 def test_big_intergers_odd():
-    capturedOutput = io.StringIO()                  # Create StringIO object
-    sys.stdout = capturedOutput                     #  and redirect stdout.
-    big_intergers(9, 29, 7, 27)                                  # Call function.
-    sys.stdout = sys.__stdout__                     # Reset redirect.
+    capturedOutput = io.StringIO()                  
+    sys.stdout = capturedOutput                     
+    big_intergers(9, 29, 7, 27)                                  
+    sys.stdout = sys.__stdout__                     
 
     assert capturedOutput.getvalue() == '4710194409608608369201743232\n', f"Printed: {capturedOutput.getvalue()}, instead of 4710194409608608369201743232"
 
 
 def test_big_intergers_even():
-    capturedOutput = io.StringIO()                  # Create StringIO object
-    sys.stdout = capturedOutput                     #  and redirect stdout.
-    big_intergers(8, 44, 2, 50)                                  # Call function.
-    sys.stdout = sys.__stdout__                     # Reset redirect.
+    capturedOutput = io.StringIO()                  
+    sys.stdout = capturedOutput                     
+    big_intergers(8, 44, 2, 50)                                  
+    sys.stdout = sys.__stdout__                     
 
     assert capturedOutput.getvalue() == '5444517870735015415413994844808198225920\n', f"Printed: {capturedOutput.getvalue()}, instead of 5444517870735015415413994844808198225920"
 
 
 def test_big_intergers_negative():
-    capturedOutput = io.StringIO()                  # Create StringIO object
-    sys.stdout = capturedOutput                     #  and redirect stdout.
-    big_intergers(-3, 24, 7, 33)                                  # Call function.
-    sys.stdout = sys.__stdout__                     # Reset redirect.
+    capturedOutput = io.StringIO()                  
+    sys.stdout = capturedOutput                     
+    big_intergers(-3, 24, 7, 33)                                  
+    sys.stdout = sys.__stdout__                     
 
     assert capturedOutput.getvalue() == '7730993719707444806566630888\n', f"Printed: {capturedOutput.getvalue()}, instead of 7730993719707444806566630888"
