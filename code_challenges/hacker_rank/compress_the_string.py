a2c6536a02423f8a45f6c0764c91609f94878e93
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-09 16:03:22 -0500

    finished HR compress the string

---
+++ code_challenges/hacker_rank/compress_the_string.py
@@ -0,0 +1,18 @@
+def compressing_strings(a_string):
+    count = 0
+    current_letter = a_string[0]
+    
+    for letter in a_string:
+        if letter == current_letter:
+            count += 1
+        else:
+            print(f'({count}, {current_letter}) ', end = '')
+            current_letter = letter
+            count = 1
+print(f'({count}, {current_letter})', end = '')
+    
+
+if __name__ == '__main__':
+    a_string = "aabbbcaafff"
+    # print(a_string)
+    compressing_strings(a_string)
