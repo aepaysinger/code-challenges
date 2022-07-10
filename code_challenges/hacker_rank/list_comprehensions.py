14755a24d180569a3be6f7f87aa906f296a13518
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-10 13:18:19 -0500

    fixing spacing for list comprehensions

--- code_challenges/hacker_rank/list_comprehensions.py
+++ code_challenges/hacker_rank/list_comprehensions.py
@@ -2,6 +2,7 @@ def create_cuboids(x, y, z, n):
     cuboids = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n]
     print(cuboids)
 
+
 if __name__ == '__main__':
     x = int(input())
     y = int(input())
