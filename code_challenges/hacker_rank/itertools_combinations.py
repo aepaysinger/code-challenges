3b58e7802d7b02b0d927f8c3744e394a779f65da
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-11 10:34:40 -0500

    still working on itertools combinations

--- code_challenges/hacker_rank/itertools_combinations.py
+++ code_challenges/hacker_rank/itertools_combinations.py
@@ -1,8 +1,26 @@
 from itertools import combinations
 
 
-def making_combinations(a_string, a_number):
-    list_a = list(combinations(a_string, a_number))
-    print(list_a.sort)
+# def making_combinations(a_string, a_number):
+#     list_a_string = list(a_string)
+#     list_a_string = sorted(list_a_string, key=str.lower)
+#     for letter in list_a_string:
+#         print(letter)
+#     list_a_string = list(combinations(list_a_string, a_number))
+#     for pairs in list_a_string:
+#         print("".join(pairs))
 
-making_combinations('HACK', 2)
+# making_combinations('HACK', 2)
+
+# def making_combinations(a_string, a_number):
+#     list_a_string = list(a_string)
+#     list_a_string = sorted(list_a_string, key=str.lower)
+#     for letter in list_a_string:
+#         print(letter)
+#     count = 0
+#     while count <= a_number:
+#         list_a_string = list(combinations(list_a_string, count))
+#         for pairs in list_a_string:
+#          print("".join(pairs))
+
+# making_combinations('ABCD', 3)
