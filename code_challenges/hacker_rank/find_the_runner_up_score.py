fa8f892ab466fbfb82c76e291f3685ccadefb0be
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-11 06:31:03 -0500

    find the runner up - added outlyer (TIE)

--- code_challenges/hacker_rank/find_the_runner_up_score.py
+++ code_challenges/hacker_rank/find_the_runner_up_score.py
@@ -1,16 +1,25 @@
+def check_tie(array):
+    return len(set(array)) == 1
+
+
 def runner_up(array):
-    biggest_number = -100
-    second_biggest = -100
-    for number in array:
-        if number > biggest_number:
-            second_biggest = biggest_number
-            biggest_number = number
-        elif number > second_biggest and number < biggest_number:
-            second_biggest = number
+    if check_tie(array) == True: 
+        print("it's a tie!")
+        return
+    else: 
+        biggest_number = -100 
+        second_biggest = -100    
+        for number in array:
+            if number > biggest_number:
+                second_biggest = biggest_number
+                biggest_number = number
+            elif number > second_biggest and number < biggest_number:
+                second_biggest = number        
     print(second_biggest)
 
 
 if __name__ == '__main__':
-    # n = 8 8 8 8
-    # arr = map(int, input().split())
-    runner_up([8, 8, 8, 8])
+    n = int(input())
+    arr = map(int, input().split())
+    check_tie(n)
+    runner_up(n)
--- tests/hacker_rank/test_find_the_runner_up_score.py
+++ tests/hacker_rank/test_find_the_runner_up_score.py
@@ -4,7 +4,7 @@ import sys
 from code_challenges.hacker_rank.find_the_runner_up_score import runner_up
 
 
-def test_find_the_runner_up_5():
+def test_runner_up_low_first():
     capturedOutput = io.StringIO()                 
     sys.stdout = capturedOutput                     
     runner_up([2, 3, 6, 6, 5])                                 
@@ -13,16 +13,16 @@ def test_find_the_runner_up_5():
     assert capturedOutput.getvalue() == "5\n", f"Printed: {capturedOutput.getvalue()}, instead of 5"
 
 
-def test_collections_counter_a():
+def test_runner_up_tie():
     capturedOutput = io.StringIO()                 
     sys.stdout = capturedOutput                     
     runner_up([8, 8, 8, 8])                                 
     sys.stdout = sys.__stdout__                    
 
-    assert capturedOutput.getvalue() == "8", f"Printed: {capturedOutput.getvalue()}, instead of 5"
+    assert capturedOutput.getvalue() == "it's a tie!\n", f"Printed: {capturedOutput.getvalue()}, instead of it's a tie"
 
 
-def test_collections_counter_a():
+def test_runner_up_high_first():
     capturedOutput = io.StringIO()                 
     sys.stdout = capturedOutput                     
     runner_up([7, 6, 3, 3, 5])                                 
