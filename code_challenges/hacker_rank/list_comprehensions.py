9dfe6969dc6a03d411784341c7f88aeb4d88502b
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-14 14:20:35 -0500

    reformated files using black

--- code_challenges/hacker_rank/list_comprehensions.py
+++ code_challenges/hacker_rank/list_comprehensions.py
@@ -1,9 +1,15 @@
 def create_cuboids(x, y, z, n):
-    cuboids = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i + j + k != n]
+    cuboids = [
+        [i, j, k]
+        for i in range(x + 1)
+        for j in range(y + 1)
+        for k in range(z + 1)
+        if i + j + k != n
+    ]
     return cuboids
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     x = int(input())
     y = int(input())
     z = int(input())
