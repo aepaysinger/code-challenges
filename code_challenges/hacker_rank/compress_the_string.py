56697d7424af7f953ba770e0ac732a53b69efca3
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-14 13:10:27 -0500

    still working on

--- code_challenges/hacker_rank/compress_the_string.py
+++ code_challenges/hacker_rank/compress_the_string.py
@@ -1,17 +1,38 @@
 def compressing_strings(a_string):
     count = 0
     current_letter = a_string[0]
+    compressed_string = []
     
     for letter in a_string:
         if letter == current_letter:
             count += 1
         else:
-            print(f'({count}, {current_letter}) ', end = '')
+            compressed_string.append((count, current_letter))
             current_letter = letter
             count = 1
-    print(f'({count}, {current_letter})', end = '')
+    return compressed_string
+    # print(f'({count}, {current_letter})', end = '')
     
 
 if __name__ == '__main__':
-    a_string = str(input())
-    compressing_strings(a_string)
+    # a_string = str(input())
+    print(compressing_strings('1222311'))
+# def compressing_strings(a_string):
+#     count = 0
+#     current_letter = a_string[0]
+
+    
+#     for letter in a_string:
+#         if letter == current_letter:
+#             count += 1
+#         else:
+#             print(f'({count}, {current_letter}) ', end = '')
+#             current_letter = letter
+#             count = 1
+#     return f'({count}, {current_letter})', end = ''
+#     print(f'({count}, {current_letter})', end = '')
+    
+
+# if __name__ == '__main__':
+#     # a_string = str(input())
+#     print(compressing_strings('1222311'))
