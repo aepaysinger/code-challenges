6fd0bfb8eb6cf53b41e6d85e7cbca18f34facde5
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-14 13:18:48 -0500

    added check tie tests

--- tests/hacker_rank/test_find_the_runner_up_score.py
+++ tests/hacker_rank/test_find_the_runner_up_score.py
@@ -1,4 +1,5 @@
 from code_challenges.hacker_rank.find_the_runner_up_score import runner_up
+from code_challenges.hacker_rank.find_the_runner_up_score import check_tie
 
 
 def test_runner_up_low_first():                    
@@ -21,4 +22,17 @@ def test_runner_up_high_first():
 
     assert actual == expected, f"Returned: {actual}, instead of {expected}"
 
-    
+
+def test_check_tie_true():
+    actual = check_tie([3, 3, 3, 3])
+    expected = True
+
+    assert actual == expected, f"Returned: {actual}, instead of {expected}"
+
+
+def test_check_tie_false():
+    actual = check_tie([1, 2, 3, 3])
+    expected = False
+
+    assert actual == expected, f"Returned: {actual}, instead of {expected}"
+ 
