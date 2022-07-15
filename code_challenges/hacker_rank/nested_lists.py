988dcd801a0063bb983cd668c82738d2a1dd1871
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-15 15:09:54 -0500

    updated nested lists

--- code_challenges/hacker_rank/nested_lists.py
+++ code_challenges/hacker_rank/nested_lists.py
@@ -1,5 +1,5 @@
 def student_grades(records):
-   
+
     lowest_score = 100
     second_lowest_score = 100
     lowest_students = []
@@ -17,13 +17,16 @@ def student_grades(records):
             second_lowest_score = student_name_and_score[1]
         elif student_name_and_score[1] == second_lowest_score:
             second_lowest_students.append(student_name_and_score[0])
-    return '\n'.join([name for name in sorted((second_lowest_students))])
-  
+    if second_lowest_students == []:
+        return "No Second Lowest"
+    else:
+        return "\n".join([name for name in sorted((second_lowest_students))])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # records = []
     # for _ in range(int(input())):
     #     name = input()
     #     score = float(input())
     #     records.append([name, score])
-    print(student_grades([['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41.0], ['Harsh', 39.0]]))
+    print(student_grades([["Ezra", 98.0], ["Oliver", 96.7], ["Rusty", 55.7]]))
--- tests/hacker_rank/test_nested_lists.py
+++ tests/hacker_rank/test_nested_lists.py
@@ -1,8 +1,32 @@
 from code_challenges.hacker_rank.nested_lists import student_grades
 
 
+def test_nested_lists_tie():
+    actual = student_grades(
+        [
+            ["Harry", 37.21],
+            ["Berry", 37.21],
+            ["Tina", 37.2],
+            ["Akriti", 41.0],
+            ["Harsh", 39.0],
+        ]
+    )
+    expected = "Berry\nHarry"
+
+    assert actual == expected, f"returned {actual} instead of {expected}"
+
+
 def test_nested_lists():
-    actual = student_grades(['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41.0], ['Harsh', 39.0])
-    expected = "Harry\nBerry"
+    actual = student_grades([["Ezra", 98.0], ["Oliver", 96.7], ["Rusty", 55.7]])
+    expected = "Oliver"
+
+    assert actual == expected, f"returned {actual} instead of {expected}"
+
+
+def test_nested_lists_no_second_lowest():
+    actual = student_grades(
+        [["Bob", 65.4], ["Mandy", 65.4], ["Lemon", 65.4], ["Frank", 65.4]]
+    )
+    expected = "No Second Lowest"
 
     assert actual == expected, f"returned {actual} instead of {expected}"
