536986a885ca858c1fb3c32d08b1ebc9b9fd3414
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-18 17:12:20 -0500

    still working on

---
+++ code_challenges/hacker_rank/designer_door_mat.py
@@ -0,0 +1,12 @@
+def draw_door_mat(size):
+    height = int(size[0])
+    length = int(size[1])
+    design = "WELCOME"
+    return height, length
+
+
+
+
+if __name__ == '__main__':
+    size = ['7', '21']
+    print(draw_door_mat(size))
