f35b1fe4857b76e4c4338169313bb4920affe451
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-20 10:22:33 -0500

    still working on and need some help

--- code_challenges/hacker_rank/designer_door_mat.py
+++ code_challenges/hacker_rank/designer_door_mat.py
@@ -1,12 +1,41 @@
 def draw_door_mat(size):
     height = int(size[0])
     length = int(size[1])
-    design = "WELCOME"
-    return height, length
+    starter = int((length - 1)/2) #5
+    greeting = "WELCOME"
+    print(starter)
+    detail = ".|."
+    for _ in range(int(height/2)):
+        draw =  "-".rjust((starter - 1),"-") + detail + "-".ljust((starter -1), "-")
+        starter -= 3
+        detail += ".|..|."
+
+        print(draw)
+    print(greeting.center(((height * 2) + len("WELCOME")),"-"))
+    starter = int(length-1)
+    detail = ".|."
+    for _ in range(int(height/2)):
+        draw =  "-".rjust((starter - 1),"-") + detail + "-".ljust((starter - 1), "-")
+        starter -= 3
+        detail += ".|..|."
+        print(draw)
+
 
 
 
 
 if __name__ == '__main__':
     size = ['7', '21']
-    print(draw_door_mat(size))
+    draw_door_mat(size)
+
+#     >>> width = 20
+# >>> print 'HackerRank'.ljust(width,'-')
+# HackerRank---------- 
+
+    # ---------.|.---------
+    # ------.|..|..|.------
+    # ---.|..|..|..|..|.---
+    # -------WELCOME-------
+    # ---.|..|..|..|..|.---
+    # ------.|..|..|.------
+    # ---------.|.---------
