0de8e0f784f48359a89e3cee7a928073ab31abb5
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-22 06:38:33 -0500

    still working on, need a little help with test cases

--- code_challenges/hacker_rank/designer_door_mat.py
+++ code_challenges/hacker_rank/designer_door_mat.py
@@ -1,70 +1,34 @@
-def draw_door_mat(size):
+def draw_door_mat(size, greeting):
     height = int(size[0])
     length = int(size[1])
-    starter = int((length - 1)/2) #5
-    greeting = "WELCOME"
-
+    starter = int((length - 1)/2) 
+    greeting_design_count = int((length - int(len(greeting))) / 2)
     detail_a = ".|."
     detail_b = "-"
     result = []
-
+    detail_b_count = int((length - 1)/2)
+    detail_a_count = 1
+  
     for n in range((int(height/2))):
-        result.append(detail * n)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # print(starter)
-    # detail = ".|."
-    # for _ in range(int(height/2)):
-    #     draw =  "-".rjust((starter - 1),"-") + detail + "-".ljust((starter -1), "-")
-    #     starter -= 3
-    #     detail += ".|..|."
-
-    #     print(draw)
-    # print(greeting.center(((height * 2) + len("WELCOME")),"-"))
-    # starter = int(length/7)
-    # print(starter)
-    # detail = ".|." * int((length//2)/2)
-    # count = 2
-    # for _ in range(int(height/2)):
-    #     draw =  "-".rjust((starter - 1),"-") + detail + "-".ljust((starter), "-")
-    #     starter += 3
-    #     detail = ".|." * (int((length/2)/2) - count)
-    #     count += 2
-            
-            
-    #     print(draw)
-
-
-
-
-
+        result.append((detail_b * (detail_b_count - 1)) + (detail_a * detail_a_count) + (detail_b * (detail_b_count - 1)))
+        detail_b_count -= 3
+        detail_a_count += 2
+    if len(greeting) % 2 ==0:
+        result.append((detail_b * (greeting_design_count + 1)) + greeting + (detail_b * greeting_design_count))
+    else:
+        result.append((detail_b * greeting_design_count) + greeting + (detail_b * greeting_design_count)) 
+         
+    detail_b_count = int(length/height)
+    
+    for n in range((int(height/2))):
+        result.append((detail_b * (detail_b_count)) + (detail_a * (detail_a_count - 2)) + (detail_b * (detail_b_count)))
+        detail_b_count += 3
+        detail_a_count -= 2
+        
+    return "\n".join(result)
 
 
 if __name__ == '__main__':
     size = ['7', '21']
-    draw_door_mat(size)
-# 7 21
-#     >>> width = 20
-# >>> print 'HackerRank'.ljust(width,'-')
-# HackerRank---------- 
-
-    # ---------.|.---------
-    # ------.|..|..|.------
-    # ---.|..|..|..|..|.---
-    # -------WELCOME-------
-    # ---.|..|..|..|..|.---
-    # ------.|..|..|.------
-    # ---------.|.---------
+    greeting = "WELCOME"
+    print(draw_door_mat(size, greeting))
---
+++ tests/hacker_rank/test_designer_door_mat.py
@@ -0,0 +1,5 @@
+from code_challenges.hacker_rank.designer_door_mat import draw_door_mat
+
+
+def test_door_mat_height_not_odd():
+    actual = draw_door_mat(['6', '18'], 'WELCOME')
