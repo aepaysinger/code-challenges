4823862eea6399d338249fd44e89ec17272a8e77
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-25 08:31:33 -0500

    think i finally worked these out
    git push

--- tests/hacker_rank/test_designer_door_mat.py
+++ tests/hacker_rank/test_designer_door_mat.py
@@ -1,5 +1,37 @@
 from code_challenges.hacker_rank.designer_door_mat import draw_door_mat
 
 
-def test_door_mat_height_not_odd():
-    actual = draw_door_mat(['6', '18'], 'WELCOME')
+def test_door_mat_height_():
+    actual = draw_door_mat(["7", "21"], "WELCOME")
+    expected = "---------.|.---------\n------.|..|..|.------\n---.|..|..|..|..|.---\n\
+-------WELCOME-------\n---.|..|..|..|..|.---\n------.|..|..|.------\n\
+---------.|.---------"
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_dppr_mat_even():
+    actual = draw_door_mat(["6", "22"], "WELCOME")
+    expected = (
+        "Mat size must be N * M . (N is an odd natural number, and M is 3 times N."
+    )
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_door_mat_odd_greeting():
+    actual = draw_door_mat(["11", "33"], "HEY")
+    expected = "---------------.|.---------------\n------------.|..|..|.------------\n---------.|..|..|..|..|.---------\n\
+------.|..|..|..|..|..|..|.------\n---.|..|..|..|..|..|..|..|..|.---\n---------------HEY---------------\n\
+---.|..|..|..|..|..|..|..|..|.---\n------.|..|..|..|..|..|..|.------\n---------.|..|..|..|..|.---------\n\
+------------.|..|..|.------------\n---------------.|.---------------"
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_door_mat_even_greeting():
+    actual = draw_door_mat(["7", "21"], "HOLA")
+    expected = "---------.|.---------\n------.|..|..|.------\n---.|..|..|..|..|.---\n\
+---------HOLA--------\n---.|..|..|..|..|.---\n------.|..|..|.------\n---------.|.---------"
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
