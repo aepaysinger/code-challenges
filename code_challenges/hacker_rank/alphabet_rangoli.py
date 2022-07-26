a5d06a6b2649279ee7734ca2a03ea056e93da8e5
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-07-26 08:32:22 -0500

    still working on

---
+++ code_challenges/hacker_rank/alphabet_rangoli.py
@@ -0,0 +1,30 @@
+def alphabet_rangoli(rangoli_size):
+    design = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+    amount_of_dashes = int(rangoli_size + (rangoli_size -1)) - 1 #4
+    amount_of_letters = int(rangoli_size + (rangoli_size - 1)) #5
+    rangoli_length = amount_of_dashes + amount_of_letters #9
+    pattern = []
+    count = amount_of_letters
+    for _ in range(rangoli_size):
+        pattern.append("-"*count + design[rangoli_size-1] + "-"*count )
+        count -= 2
+    return pattern
+
+    
+
+
+if __name__ == "__main__":
+    rangoli_size = 3
+    print(alphabet_rangoli(rangoli_size))
+
+
+ #size + (size -1) - 1 = dashes, 18
+ # size + size - 1 = letters, 19
+ # dashes + letters = len
+    #size 3
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
