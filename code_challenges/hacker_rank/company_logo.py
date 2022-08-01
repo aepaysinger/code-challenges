69b9bd5eae87cefc8a21896234f0dd00ac23f5c7
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-08-01 09:51:43 -0500

    finished it! added some outliers and updated the function to adapt

--- code_challenges/hacker_rank/company_logo.py
+++ code_challenges/hacker_rank/company_logo.py
@@ -1,32 +1,31 @@
 def make_logo(name):
     letters = {}
     for letter in name:
-        if letter not in letters:
-            letters[letter] = 1
-        else:
+        if letter == " ":
+            letters[letter] = 0
+            continue
+        try:
             letters[letter] += 1
-    top1 = ["",0]
-    top2 = ["",0]
-    top3 = ["",0]
-    for letter in letters:
+        except KeyError:
+            letters[letter] = 1
+
+    top1 = ("", 0)
+    top2 = ("", 0)
+    top3 = ("", 0)
+    for letter in sorted(letters):
         if letters[letter] > top1[1]:
             top3 = top2
             top2 = top1
-            top1 = [letter, letters[letter]]
-        elif letters[letter] >top2[1]:
+            top1 = (letter, letters[letter])
+        elif letters[letter] > top2[1]:
             top3 = top2
-            top2 = [letter, letter[letter]]
+            top2 = (letter, letters[letter])
         elif letters[letter] > top3[1]:
-            top3 = [letter, letters[letter]]
-        else:
-            pass
-    return top1[0], letters[top1[0]]
+            top3 = (letter, letters[letter])
 
-
-    
-
+    return f"{' '.join(map(str, top1))}\n{' '.join(map(str, top2))}\n{' '.join(map(str, top3))}"
 
 
 if __name__ == "__main__":
-    name = "aabbbccde"
+    name = "crazy company name"
     print(make_logo(name))
