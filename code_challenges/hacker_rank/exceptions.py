f6c63bc1bce38029fe035b3967e7e047cd247cd9
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-08-01 10:14:55 -0500

    fixing

--- code_challenges/hacker_rank/exceptions.py
+++ code_challenges/hacker_rank/exceptions.py
@@ -1,8 +1,8 @@
-def find_the_exception(number_of_test_cases, the_values):
+def find_the_exception(the_values):
     result = []
     for first, second in the_values:
         try:
-            result.append(int(int(first) / int(second)))
+            result.append(str(int(int(first) / int(second))))
         except ZeroDivisionError:
             result.append("Error Code: integer division or modulo by zero")
         except ValueError:
@@ -14,12 +14,11 @@ def find_the_exception(number_of_test_cases, the_values):
                 result.append(
                     f"Error Code: invalid literal for int() with base 10: '{first}'"
                 )
-    return "\n".join(map(str, result))
+    return "\n".join(result)
 
 
 if __name__ == "__main__":
-    number_of_test_cases = int(input())
-    the_values = []
-    for _ in range(number_of_test_cases):
-        the_values.append(input().split())
-    print(find_the_exception(number_of_test_cases, the_values))
+    the_values = [["1", "0"], ["2", "$"], ["3", "1"]]
+    # for _ in range(number_of_test_cases):
+    #     the_values.append(input().split())
+    print(find_the_exception(the_values))
