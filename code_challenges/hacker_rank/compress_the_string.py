c3d36505b23bb4d309f3c5509aa8cab120e5c520
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-08-02 06:03:28 -0500

    complete?

--- code_challenges/hacker_rank/compress_the_string.py
+++ code_challenges/hacker_rank/compress_the_string.py
@@ -1,38 +1,18 @@
 def compressing_strings(a_string):
     count = 0
     current_letter = a_string[0]
-    compressed_string = []
-    
+    compressed_string = ""
+
     for letter in a_string:
         if letter == current_letter:
             count += 1
         else:
-            compressed_string.append((count, current_letter))
+            compressed_string += f"({count}, {current_letter}) "
             current_letter = letter
             count = 1
-    return ",".join(compressed_string)
-    
-    
+    return f"{compressed_string}({count}, {current_letter})"
 
-if __name__ == '__main__':
-    # a_string = str(input())
-    print(compressing_strings('1222311'))
-# def compressing_strings(a_string):
-#     count = 0
-#     current_letter = a_string[0]
 
-    
-#     for letter in a_string:
-#         if letter == current_letter:
-#             count += 1
-#         else:
-#             print(f'({count}, {current_letter}) ', end = '')
-#             current_letter = letter
-#             count = 1
-#     return f'({count}, {current_letter})', end = ''
-#     print(f'({count}, {current_letter})', end = '')
-    
-
-# if __name__ == '__main__':
-#     # a_string = str(input())
-#     print(compressing_strings('1222311'))
+if __name__ == "__main__":
+    a_string = str(input())
+    print(compressing_strings(a_string))
--- tests/hacker_rank/test_compress_the_string.py
+++ tests/hacker_rank/test_compress_the_string.py
@@ -1,31 +1,22 @@
-import io
-import sys
-
 from code_challenges.hacker_rank.compress_the_string import compressing_strings
 
 
 def test_compressing_strings_letters():
-    capturedOutput = io.StringIO()                 
-    sys.stdout = capturedOutput                    
-    compressing_strings("aabbbcaafff")                                  
-    sys.stdout = sys.__stdout__                     
+    actual = compressing_strings("aabbbcaafff")
+    expected = "(2, a) (3, b) (1, c) (2, a) (3, f)"
 
-    assert capturedOutput.getvalue() == "(2, a) (3, b) (1, c) (2, a) (3, f)", f"Printed: {capturedOutput.getvalue()}, instead of (2, a) (3, b) (1, c) (2, a) (3, f)"
+    assert actual == expected, f"Returned: {actual}, instead of {expected}"
 
 
 def test_compressing_strings_numbers():
-    capturedOutput = io.StringIO()                 
-    sys.stdout = capturedOutput                    
-    compressing_strings("1113338844331")                                  
-    sys.stdout = sys.__stdout__                     
+    actual = compressing_strings("1113338844331")
+    expected = "(3, 1) (3, 3) (2, 8) (2, 4) (2, 3) (1, 1)"
 
-    assert capturedOutput.getvalue() == "(3, 1) (3, 3) (2, 8) (2, 4) (2, 3) (1, 1)", f"Printed: {capturedOutput.getvalue()}, instead of (3, 1) (3, 3) (2, 8) (2, 4) (2, 3) (1, 1)"   
+    assert actual == expected, f"Returned: {actual}, instead of {expected}"
 
 
 def test_compressing_strings_letter_and_numbers():
-    capturedOutput = io.StringIO()                 
-    sys.stdout = capturedOutput                    
-    compressing_strings("aa888n333dddd")                                  
-    sys.stdout = sys.__stdout__                     
+    actual = compressing_strings("aa888n333dddd")
+    expected = "(2, a) (3, 8) (1, n) (3, 3) (4, d)"
 
-    assert capturedOutput.getvalue() == "(2, a) (3, 8) (1, n) (3, 3) (4, d)", f"Printed: {capturedOutput.getvalue()}, instead of (2, a) (3, 8) (1, n) (3, 3) (4, d)" 
+    assert actual == expected, f"Returned: {actual}, instead of {expected}"
