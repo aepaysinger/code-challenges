1461bf5a54e12f696066204ab1bffa8b08eefbae
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-08-11 10:51:15 -0500

    still working on

--- code_challenges/hacker_rank/detect_floating_point_number.py
+++ code_challenges/hacker_rank/detect_floating_point_number.py
@@ -3,17 +3,34 @@ def detect_floating_point_number(cases):
     for case in cases:
         if "+" in case[1:]:
             results.append("False")
-        if case.isalpha():
+        elif case.isalpha():
             results.append("False")   
-        if case[0] == "-" or case[0] == "+":
+        elif case[0] == "-" or case[0] == "+":
             case = case.replace('-', '')
             case = case.replace('+', '')
+            #what happens with these strings?
         elif case.isdigit():
             results.append("False")
         else:
             results.append("True")
         
     return  "\n".join(results)
+# def detect_floating_point_number(case):
+    
+    
+#     if "+" in case[1:]:
+#         print("False")
+#     elif case.isalpha():
+#         print("False")   
+#     elif case[0] == "-" or case[0] == "+":
+#         case = case.replace('-', '')
+#         case = case.replace('+', '')
+#     elif case.isdigit():
+#         print("False")
+#     else:
+#         print("True")
+        
+    # return  "\n".join(results)
         
 
 if __name__ == "__main__":
@@ -38,3 +55,21 @@ if __name__ == "__main__":
 #                 results.append("False")
         
 #     return  "\n".join(results)
+
+
+
+def detect_floating_point_number(cases):
+    results = []
+    for case in cases:
+        if case[0] == "-" or case[0] == "+":
+            # we either need to append to results inside here
+            # or: this needs to not be part of the main if/elif block
+            case = case[1:]
+        
+        if "+" in case or case.isalpha():
+            results.append("False")
+        elif case.count(".") != 1:
+            results.append("False")
+        else:
+            results.append("True")
+    return  "\n".join(results)
