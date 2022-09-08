525b884caa112e75f41b17c97d71a656ab1c3372
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-08 14:01:38 -0500

    cw consecutive strings, working on

--- code_challenges/code_wars/consecutive_strings.py
+++ code_challenges/code_wars/consecutive_strings.py
@@ -9,18 +9,9 @@ def consecutive_strings(starr, k):
 
     longest = 0
     consecutive_string = ""
-    for _ in range(k):
-        for key in word_tracker:
-            if key > longest:
-                longest = key
-                word_tracker[longest] += consecutive_string
-                del word_tracker[longest]
-                longest = 0
+    
 
        
-        
-        
-
     return word_tracker, consecutive_string
 
     
