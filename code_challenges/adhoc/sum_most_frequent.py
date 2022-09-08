f92d042ea3222b4325a637f7136d058fe6d492e1
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-08 09:51:24 -0500

    sum most frequent completed with tests

--- code_challenges/adhoc/sum_most_frequent.py
+++ code_challenges/adhoc/sum_most_frequent.py
@@ -16,18 +16,13 @@ def sum_most_frequent_numbers(numbers):
             num_b_count = num_a_count
             num_a = number
             num_a_count = counts[number]
-        
-# 
+        elif counts[number] >= num_b_count:
+            num_b_count = counts[number]
+            num_b = number
 
-
-    return num_a+num_b
-
-#l = [1, 1, 1, 1, 2, 2]     
-
-
+    return num_a + num_b
 
 
 if __name__ == "__main__":
-    l = [1, 4, 3, 3, 7, 2, 2, 9, 9]
+    l = [1, 4, 3, 3, 7, 2, 2, 9]
     print(sum_most_frequent_numbers(l))
-    # return 5
