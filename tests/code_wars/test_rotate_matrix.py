ecb1c9ba88488d8296ee06d3add3a038179c1ba9
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-08 15:09:33 -0500

    updated the tests for rotate the matrix

--- tests/code_wars/test_rotate_matrix.py
+++ tests/code_wars/test_rotate_matrix.py
@@ -3,16 +3,16 @@ from code_challenges.code_wars.rotate_matrix import rotate_matrix
 
 def test_rotate_matrix_4_x_4():
     actual = rotate_matrix([
-        [1,2,3,8],
-        [4,5,6,8],
-        [7,8,9,8],
-        [9,8,7,6],
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
     ])
     expected = [
-        [9, 7, 4, 1],
-        [8, 8, 5, 2],
-        [7, 9, 6, 3],
-        [6, 8, 8, 8],
+        [13, 9, 5, 1],
+        [14, 10, 6, 2],
+        [15, 11, 7, 3],
+        [16, 12, 8, 4],
     ]
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
