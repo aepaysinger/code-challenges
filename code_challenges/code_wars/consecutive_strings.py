5bc6da2e9c766989678f3b4887e9af82d2e2aa33
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-11 15:20:50 -0500

    CW consecustive string completed

--- code_challenges/code_wars/consecutive_strings.py
+++ code_challenges/code_wars/consecutive_strings.py
@@ -6,20 +6,29 @@ def consecutive_strings(starr, k):
             word_tracker[len(word)] = [word]
         else:
             word_tracker[len(word)] += [word]
-
-    longest = 0
-    consecutive_string = ""
+    print(word_tracker)
     
+    the_words = []
 
+    
+    for _ in range(len(word_tracker)):
+        amount = 0
+        for word_length in word_tracker:
+            if word_length > amount:
+                amount = word_length
+        the_words += word_tracker[amount]
+        del word_tracker[amount]
+        amount = 0
+        
        
-    return word_tracker, consecutive_string
-
     
-
-
-
-
+    final_string = ""
+    for i in range(k):
+        final_string += the_words[i]
+       
+    return  final_string
+    
 if __name__ == "__main__":
-    starr = ["tree", "foling", "trashy", "blue", "abcdef", "uvwxyz"]
+    starr = ["zone", "abigail", "theta", "form", "libe", "zas"]
     k = 2
     print(consecutive_strings(starr, k)) 
---
+++ tests/code_wars/test_consecutive_strings.py
@@ -0,0 +1,20 @@
+from code_challenges.code_wars.consecutive_strings import consecutive_strings
+
+
+def test_consecutive_strings_2():
+    actual = consecutive_strings(["zone", "abigail", "theta", "form", "libe", "zas"], 2)
+    expected = "abigailtheta"
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_consecutive_strings_3():
+    actual = consecutive_strings(["zone", "abigail", "theta", "form", "libe", "zas"], 3)
+    expected = "abigailthetazone"
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_consecutive_strings_same_length():
+    actual = consecutive_strings(["auto", "zone", "love", "them", "cars", "man!"], 2)
+    expected = "autozone"
