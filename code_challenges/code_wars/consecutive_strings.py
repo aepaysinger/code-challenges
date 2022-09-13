90153beec812adb7a71fa56e13cde24413c49f65
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-13 12:46:53 -0500

    cw consecutive strings completed with readme

--- code_challenges/code_wars/consecutive_strings.py
+++ code_challenges/code_wars/consecutive_strings.py
@@ -1,40 +1,27 @@
 def consecutive_strings(starr, k):
-    if k > len(starr) or k <= 0 or len(starr) == 0:
-        return ""
-    word_tracker = {}
-    
-    for word in starr:
-        if word_tracker.get(len(word)) == None:
-            word_tracker[len(word)] = [word]
-        else:
-            word_tracker[len(word)] += [word]
-    print(word_tracker)
-    
-    the_words = []
-    for _ in range(len(word_tracker)):
-        amount = 0
-        for word_length in word_tracker:
-            if word_length > amount:
-                amount = word_length
-        the_words += word_tracker[amount]
-        del word_tracker[amount]
-        amount = 0
+    length = 0
+    concatenated_string = ""
+    for word in range(0, len(starr), 1):
+        word_groups = starr[word: word + k]
+        if len(word_groups) == k:
+            words = "".join(word_groups)
+            if len(words) > length:
+                length = len(words)
+                concatenated_string = words
+
+    return concatenated_string
         
-    final_words = []
-    for i in range(k):
-        final_words.append(the_words[i])
-
-    final_string = ""
-    for word in starr:
-        if word in final_words:
-            final_string += word
-
-       
-    return  final_string
+        
+        
+        
     
 if __name__ == "__main__":
     starr = ["zone", "theta", "abigail", "form", "libe", "zas"]
     k = 2
     print(consecutive_strings(starr, k)) 
 
-   # return in the order they came in"xx
+
+
+#    l = [1, 2, 3, 4, 5, 6, 7, 8]
+# for j in range(0, len(l), 2):
+#         print(l[j: j + 2])
