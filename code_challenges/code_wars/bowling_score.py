c87de4861eb2709988c41ba91290bd969cb41dbf
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-14 13:53:41 -0500

    cw bowling score, working on a perfect score

--- code_challenges/code_wars/bowling_score.py
+++ code_challenges/code_wars/bowling_score.py
@@ -11,21 +11,38 @@ def bowling_score(frames):
         "7": 7,
         "8": 8,
         "9": 9,
-        "X": 10,
-        "/": 10,
     }
-    for round in frames:
-        score += points[round[0]] + points[round[0]]
     for i, round in enumerate(frames):
-        if round == "X":
-            score += frames[i]
-    
-        
-        
-
-    return frames
+        if len(frames[i]) == 2:
+            if frames[i][1] == "/":
+                score += 10 + (points[frames[i+1][0]])
+            else:
+                score += points[frames[i][0]] + points[frames[i][1]]
+        if len(frames[i]) == 3:
+            if frames[i] == "XXX":
+                score += 30
+        if frames[i] == "X":
+            if i == 7:
+                if frames[i+1] == "X" and frames[i+2][0] == "X":
+                    score += 30
+                    
+            if i == 8:    
+                if frames[i+1][0] == "X" and frames[i+1][1] == "X":
+                    score += 30
+                    
+            # if i == 9:
+            #     if frames[i+1][0] == "X" and frames[i+1][1] == "X" and frames[i+1][2] == "X":
+            #         score += 30
+            elif frames[i+1] == "X" and frames[i+2] == "X":
+                score += 30
+            elif frames[i+1] == "X":
+                score += 20
+            elif frames[i+1] != "X" and i != 7 or 8 or 9:
+                score += 10 + (points[frames[i+1][0]] + points[frames[i+1][1]])
+        print(frames[i], i, score)
+    return score
 
 
 if __name__ == "__main__":
-    frames = "11 X 11 11 11 11 11 11 11 11"
+    frames = "X X X X X X X X X XXX"
     print(bowling_score(frames))
