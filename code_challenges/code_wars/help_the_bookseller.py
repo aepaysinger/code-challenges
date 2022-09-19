51467c2d9b908423c01786c078f713c5b916445b
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-19 11:11:19 -0500

    CW help the bookseller, ran black

--- code_challenges/code_wars/help_the_bookseller.py
+++ code_challenges/code_wars/help_the_bookseller.py
@@ -1,6 +1,6 @@
 def help_the_bookseller(codes, categories):
     category_with_amount = category_amount(codes)
-    
+
     category_tracker = 0
     final_result = ""
     for letter in categories:
@@ -13,7 +13,7 @@ def help_the_bookseller(codes, categories):
         return ""
     else:
         return final_result[:-3]
-    
+
 
 def category_amount(codes):
     category_with_amount = {}
@@ -24,12 +24,9 @@ def category_amount(codes):
         else:
             category_with_amount[new_code[0][0]] = int(new_code[1])
     return category_with_amount
-    
-        
+
+
 if __name__ == "__main__":
     codes = ["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"]
     categories = ["E", "F", "G", "H"]
     print(help_the_bookseller(codes, categories))
-    
-
-   
