b2bcb25d683cc819ad2fa90bc153700f0f987c59
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-26 15:13:29 -0500

    class counter, tests, and methods

---
+++ code_challenges/adhoc/counter.py
@@ -0,0 +1,15 @@
+# create a counter that has an incriment , decremet, and value method (return current value)
+class Counter:
+    def __init__(self):
+        self._value = 0
+        pass
+
+    def value(self):
+        value = self._value
+        return value
+
+    def increment(self):
+        self._value += 1
+
+    def decrement(self):
+        self._value -= 1
