cb91908e7ce958eb9ea55e049246cd1e8dc2dfec
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-28 13:35:55 -0500

    CW trigrams, final changes

--- code_challenges/code_wars/trigrams.py
+++ code_challenges/code_wars/trigrams.py
@@ -1,17 +1,16 @@
 def trigrams(phrase):
-    j = 3
+    char_per_trigram = 3
     trigrams = []
 
     updated_phrase = phrase.replace(" ", "_")
-    
-    for i in range(len(updated_phrase)):
-        if len(updated_phrase[i:j]) == 3:
-            trigrams.append(updated_phrase[i:j])
-            j += 1
-        else:
-            return " ".join(trigrams)
+
+    for i in range(len(updated_phrase) - char_per_trigram + 1):
+
+        trigrams.append(updated_phrase[i : i + char_per_trigram])
+
+    return " ".join(trigrams)
 
 
 if __name__ == "__main__":
-    phrase = "divided by 3"
+    phrase = "divided by 3"  
     print(trigrams(phrase))
