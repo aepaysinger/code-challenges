4713e16166b8aea3da5be5121c6174f7ac249c29
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-28 12:01:20 -0500

    CW trigrams, edited and updated return statement

--- code_challenges/code_wars/trigrams.py
+++ code_challenges/code_wars/trigrams.py
@@ -5,13 +5,14 @@ def trigrams(phrase):
     updated_phrase = phrase.replace(" ", "_")
     while i < len(phrase):
         for character in updated_phrase:
-            
+            print(i, j, updated_phrase[i:j])
             trigrams.append(updated_phrase[i:j])
             i += 1
             j += 1
 
-    return " ".join([trigram for trigram in trigrams if len(trigram) == 3])
+    return " ".join(trigrams[:-2])
+
 
 if __name__ == "__main__":
-    phrase = "the quick red"
+    phrase = "divided by 3"
     print(trigrams(phrase))
--- tests/code_wars/test_trigrams.py
+++ tests/code_wars/test_trigrams.py
@@ -6,4 +6,17 @@ def test_trigrams():
     expected = "the he_ e_q _qu qui uic ick ck_ k_r _re red"
 
     assert actual == expected, f"Returned {actual}, instead of {expected}"
-    
+
+
+def test_trigrams_a():
+    actual = trigrams("hey this is tough")
+    expected = "hey ey_ y_t _th thi his is_ s_i _is is_ s_t _to tou oug ugh"
+
+    assert actual == expected, f"Returned {actual}, instead of {expected}"
+
+
+def test_trigrams_b():
+    actual = trigrams("divided by 3")
+    expected = "div ivi vid ide ded ed_ d_b _by by_ y_3"
+
+    assert actual == expected, f"Returned {actual}, instead of {expected}"
