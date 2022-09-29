f73a7506c9fe18fe6c22aff2ac4ca9144c631afb
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-29 13:00:08 -0500

    CW first non repeating letter, this one on a different branch. cleaned up and finished. added init so i could run tests

--- code_challenges/code_wars/first_non_repeating_letter.py
+++ code_challenges/code_wars/first_non_repeating_letter.py
@@ -1,12 +1,13 @@
 def first_non_repeating_letter(word):
-    for letter in word:
-        if word.find(letter):
+    word = list(word)
+    for i, letter in enumerate(word):
+        if letter in word[i + 1 :: 1] or letter in word[0:i]:
+            print(letter, word[i + 1 :: 1], word[0:i])
             continue
         else:
             return letter
 
 
-
 if __name__ == "__main__":
-    word = "stress"
+    word = "abcdabcde"
     print(first_non_repeating_letter(word))
