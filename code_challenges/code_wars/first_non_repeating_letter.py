540d538a3f6425df82707cf4cb4bd47c6efba12d
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-09-29 14:12:28 -0500

    CW first non repeating number, reworking

--- code_challenges/code_wars/first_non_repeating_letter.py
+++ code_challenges/code_wars/first_non_repeating_letter.py
@@ -1,10 +1,7 @@
 def first_non_repeating_letter(word):
     word = list(word)
     for i, letter in enumerate(word):
-        if letter in word[i + 1 :: 1] or letter in word[0:i]:
-            print(letter, word[i + 1 :: 1], word[0:i])
-            continue
-        else:
+        if letter not in word[i + 1 :: 1] or letter not in word[0:i]:
             return letter
 
 
