480c7a8443d282ef721398d2e46e2283a905afea
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-10-03 13:35:55 -0500

    CW-consecutive strings, updated

--- code_challenges/code_wars/consecutive_strings.py
+++ code_challenges/code_wars/consecutive_strings.py
@@ -1,18 +1,19 @@
-def consecutive_strings(starr, k):
+def consecutive_strings(names, k):
+    if k > len(names):
+        return ""
     length = 0
     concatenated_string = ""
-    for word in range(0, len(starr), 1):
-        word_groups = starr[word : word + k]
-        if len(word_groups) == k:
-            words = "".join(word_groups)
-            if len(words) > length:
-                length = len(words)
-                concatenated_string = words
+    for i in range(0, k, 1):
+        word_groups = names[i : i + k]
+        words = "".join(word_groups)
+        if len(words) > length:
+            length = len(words)
+            concatenated_string = words
 
     return concatenated_string
 
 
 if __name__ == "__main__":
-    starr = ["zone", "theta", "abigail", "form", "libe", "zas"]
+    names = ["zone", "theta", "abigail", "form", "libe", "zas"]
     k = 2
-    print(consecutive_strings(starr, k))
+    print(consecutive_strings(names, k))
