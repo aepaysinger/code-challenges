a5e9e59bfadcac723997630ee7846b07addb3208
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-10-03 14:56:41 -0500

    CW song, updating

--- code_challenges/code_wars/Song.py
+++ code_challenges/code_wars/Song.py
@@ -2,16 +2,14 @@ class Song:
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
-
+        self.names_of_listeners = set()
 
     def how_many(self, listeners):
         number_of_new_listeners = 0
-
         for name in listeners:
-            names_of_listeners = []
-            name.lower()
-            if name not in names_of_listeners:
+            name = name.lower()
+            if name not in self.names_of_listeners:
                 number_of_new_listeners += 1
-                names_of_listeners.append(name)
+                self.names_of_listeners.add(name)
 
         return number_of_new_listeners
--- tests/code_wars/test_Song.py
+++ tests/code_wars/test_Song.py
@@ -1,8 +1,8 @@
-from code_challenges.code_wars.Song import Song
+from code_challenges.code_wars.song import Song
 
 
-def test_Song():
-    mount_moose = Song('Mount Moose', 'The Snazzy Moose')
-    
-    assert mount_moose.how_many(['John', 'Fred', 'BOb', 'carl', 'RyAn']) == 5
-    assert mount_moose.how_many(['JoHn', 'Luke', 'AmAndA']) == 2
+def test_song():
+    mount_moose = Song("Mount Moose", "The Snazzy Moose")
+
+    assert mount_moose.how_many(["John", "Fred", "BOb", "carl", "RyAn"]) == 5
+    assert mount_moose.how_many(["JoHn", "Luke", "AmAndA"]) == 2
