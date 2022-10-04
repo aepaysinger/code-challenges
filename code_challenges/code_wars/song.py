772c1cd497ccb09524f6a274dcbb322a2401c454
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-10-04 09:24:19 -0500

    CW Song, updated

--- code_challenges/code_wars/song.py
+++ code_challenges/code_wars/song.py
@@ -2,14 +2,14 @@ class Song:
     def __init__(self, title, artist):
         self.title = title
         self.artist = artist
-        self.names_of_listeners = set()
+        self._names_of_listeners = set()
 
     def how_many(self, listeners):
         number_of_new_listeners = 0
         for name in listeners:
             name = name.lower()
-            if name not in self.names_of_listeners:
+            if name not in self._names_of_listeners:
                 number_of_new_listeners += 1
-                self.names_of_listeners.add(name)
+                self._names_of_listeners.add(name)
 
         return number_of_new_listeners
