117bcfcf86a2fbd3d301a510ca57de06c6a7a31b
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-10-19 11:01:19 -0500

    CW football cards, reworking

--- code_challenges/code_wars/football_cards.py
+++ code_challenges/code_wars/football_cards.py
@@ -1,53 +1,60 @@
+import re
 def football_cards(team_cards):
     a_players = 11
     b_players = 11
     a_cards = {}
     b_cards = {}
-    while a_players > 6 and b_players > 6:
-        for card in team_cards:
-            if a_players == 6 or b_players == 6:
-                break
-            if card[0] == "A":
-                if len(card) == 3:
-                    player_number = card[1]
-                    a_cards[player_number] = a_cards.get(player_number, "") + card[2]
-                    if (
-                        a_cards[player_number] == "R"
-                        or a_cards[player_number] == "YY"
-                        or a_cards[player_number] == "YR"
-                    ):
-                        a_players -= 1
-                else:
-                    player_number = card[1] + card[2]
-                    a_cards[player_number] = a_cards.get(player_number, "") + card[3]
-                    if (
-                        a_cards[player_number] == "R"
-                        or a_cards[player_number] == "YY"
-                        or a_cards[player_number] == "YR"
-                    ):
-                        a_players -= 1
-            else:
-                if len(card) == 3:
-                    player_number = card[1]
-                    b_cards[player_number] = b_cards.get(player_number, "") + card[2]
-                    if (
-                        b_cards[player_number] == "R"
-                        or b_cards[player_number] == "YY"
-                        or b_cards[player_number] == "YR"
-                    ):
-                        b_players -= 1
-                else:
-                    player_number = card[1] + card[2]
-                    b_cards[player_number] = b_cards.get(player_number, "") + card[3]
-                    if (
-                        b_cards[player_number] == "R"
-                        or b_cards[player_number] == "YY"
-                        or b_cards[player_number] == "YR"
-                    ):
-                        b_players -= 1
-        return (a_players, b_players)
 
-    return (a_players, b_players)
+    for card in team_cards:
+        match = re.match(r"([AB])([0-9]+)([YR])", card)
+        team, player, penalty = match.groups()
+        print(team, player, penalty)
+
+    
+    # for card in team_cards:
+    #     if a_players == 6 or b_players == 6:
+    #         break
+    #     if card[0] == "A":
+    #         if len(card) == 3:
+    #             player_number = card[1]
+    #             a_cards[player_number] = a_cards.get(player_number, "") + card[2]
+    #             if (
+    #                 a_cards[player_number] == "R"
+    #                 or a_cards[player_number] == "YY"
+    #                 or a_cards[player_number] == "YR"
+    #                 ):
+    #                 a_players -= 1
+    #         else:
+    #             player_number = card[1] + card[2]
+    #             a_cards[player_number] = a_cards.get(player_number, "") + card[3]
+    #             if (
+    #                 a_cards[player_number] == "R"
+    #                 or a_cards[player_number] == "YY"
+    #                 or a_cards[player_number] == "YR"
+    #             ):
+    #                 a_players -= 1
+    #     else:
+    #         if len(card) == 3:
+    #             player_number = card[1]
+    #             b_cards[player_number] = b_cards.get(player_number, "") + card[2]
+    #             if (
+    #                 b_cards[player_number] == "R"
+    #                 or b_cards[player_number] == "YY"
+    #                 or b_cards[player_number] == "YR"
+    #                 ):
+    #                 b_players -= 1
+    #         else:
+    #             player_number = card[1] + card[2]
+    #             b_cards[player_number] = b_cards.get(player_number, "") + card[3]
+    #             if (
+    #                 b_cards[player_number] == "R"
+    #                 or b_cards[player_number] == "YY"
+    #                 or b_cards[player_number] == "YR"
+    #                 ):
+    #                 b_players -= 1
+    # return (a_players, b_players)
+
+    
 
 
 if __name__ == "__main__":
