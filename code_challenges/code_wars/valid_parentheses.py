88c5d1b2ab952d621ac487ec88edd69090882e8f
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-11-11 12:17:13 -0600

    CW valid parentheses, updated

--- code_challenges/code_wars/valid_parentheses.py
+++ code_challenges/code_wars/valid_parentheses.py
@@ -1,20 +1,23 @@
 def valid_parentheses(characters):
-    open = 0
-    close = 0
+    # open = 0
+    # close = 0
     count = 0
     for character in characters:
         if character == "(":
-            open += 1
+            # open += 1
             count += 1
         elif character == ")":
-            close += 1
+            # close += 1
             count -= 1
         if count == -1:
             return False
-    if open == close:
-        return True
-    else:
+    if count == 1:
         return False
+    # if open == close:
+    #     return True
+    # else:
+    #     return False
+    return True
 
 
 if __name__ == "__main__":
