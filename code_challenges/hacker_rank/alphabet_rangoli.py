2d475e367ab33db6c2ed45dcf5fdd443ab9fd6c9
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-11-21 15:06:20 -0600

    HR alphabeth rangoli, still working on

--- code_challenges/hacker_rank/alphabet_rangoli.py
+++ code_challenges/hacker_rank/alphabet_rangoli.py
@@ -1,14 +1,22 @@
 def alphabet_rangoli(rangoli_size):
     design = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
-    amount_of_dashes = int(rangoli_size + (rangoli_size -1)) - 1 #4
-    amount_of_letters = int(rangoli_size + (rangoli_size - 1)) #5
-    rangoli_length = amount_of_dashes + amount_of_letters #9
-    pattern = []
-    count = amount_of_letters
-    for _ in range(rangoli_size):
-        pattern.append("-"*count + design[rangoli_size-1] + "-"*count )
-        count -= 2
-    return "\n".join(pattern)
+    # amount_of_dashes = int(rangoli_size + (rangoli_size -1)) - 1 #4
+    # amount_of_letters = int(rangoli_size + (rangoli_size - 1)) #5
+    # rangoli_length = amount_of_dashes + amount_of_letters #9
+    # pattern = []
+    # count = amount_of_letters
+    # for _ in range(rangoli_size):
+    #     pattern.append("-"*count + design[rangoli_size-1] + "-"*count )
+    #     count -= 2
+    # return "\n".join(pattern)
+#the length of rangoli is ((size - 1) * 4) + 1
+#the amount of rows is double the size - 1
+    pattern = [design[(rangoli_size - 1)]]
+    previouse_pattern = []
+    for i in range(rangoli_size):
+
+        pattern.append(design[(rangoli_size - 1) - i])
+    print(pattern)
 
     
 
