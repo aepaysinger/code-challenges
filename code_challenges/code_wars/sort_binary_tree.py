9243932bec0472e88c3b798fb5fb2fcb6e9a9b5b
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-11-30 09:48:47 -0600

    CW sort binary tree, still working on

---
+++ code_challenges/code_wars/sort_binary_tree.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
---
+++ tests/code_wars/test_sort_binary_tree.py
@@ -0,0 +1,8 @@
+from code_challenges.code_wars.sort_binary_tree import Node, sort_binary_tree
+
+
+def test_sort_binary_tree_a():
+    actual = sort_binary_tree()
+    expected =
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
