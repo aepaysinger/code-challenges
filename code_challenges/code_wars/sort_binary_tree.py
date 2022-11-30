1b29626e4a737c2b54d8d9d73639dd5145f5b608
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-11-30 10:11:41 -0600

    CW sort binary tree, still working on

--- code_challenges/code_wars/sort_binary_tree.py
+++ code_challenges/code_wars/sort_binary_tree.py
@@ -3,3 +3,6 @@ class Node:
         self.left = L
         self.right = R
         self.value = n
+(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1))   
+[1, 2, 3, 4, 5, 6]
+
--- tests/code_wars/test_sort_binary_tree.py
+++ tests/code_wars/test_sort_binary_tree.py
@@ -2,7 +2,7 @@ from code_challenges.code_wars.sort_binary_tree import Node, sort_binary_tree
 
 
 def test_sort_binary_tree_a():
-    actual = sort_binary_tree()
-    expected =
+    actual = sort_binary_tree(Node(Node(None, Node(None, None, 4), 2), Node(Node(None, None, 5), Node(None, None, 6), 3), 1))
+    expected =[1, 2, 3, 4, 5, 6]
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
