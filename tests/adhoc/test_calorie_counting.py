adfcd9edd378f2185a2be1f9852309db4fd938f1
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-02 14:24:56 -0600

    advent calorie counting still working on

---
+++ tests/adhoc/test_calorie_counting.py
@@ -0,0 +1,8 @@
+from code_challenges.adhoc.calorie_counting import get_elves_calories
+
+
+def test_calorie_counting():
+    actual = get_elves_calories(['1000,2000,3000', '4000', '5000,6000', '7000,8000,9000', '10000'])
+    expected = 24000
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
