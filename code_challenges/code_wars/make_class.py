517ea157cc57657ebd13e162fae826315a995e22
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-06 13:47:19 -0600

    CW make class

--- code_challenges/code_wars/make_class.py
+++ code_challenges/code_wars/make_class.py
@@ -1,10 +1,10 @@
 class Animal():
     def __init__(self, name, species, age, health, weight, color):
-    self.name = name
-    self.species = species
-    self.age = age
-    self.health = health
-    self.weight = weight
-    self.color = color
+        self.name = name
+        self.species = species
+        self.age = age
+        self.health = health
+        self.weight = weight
+        self.color = color
 
 
