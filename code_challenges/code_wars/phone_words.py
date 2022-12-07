75e5324799d9cd30a5b5b4dacee4b73a5e4c54dd
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-07 09:49:02 -0600

    CW phone words

---
+++ code_challenges/code_wars/phone_words.py
@@ -0,0 +1,16 @@
+class Phone():
+    def __init__(self, numbers):
+        self.numbers = numbers
+
+
+    def translate_text(self, numbers):
+        translator = {
+            "a": 2,
+            "b": 22,
+            "c": 222,
+            "d": 3,
+            "e": 33,
+            "f": 333,
+            "g": 4,
+            "" 
+        }
