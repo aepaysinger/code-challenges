8595012105c8d4a939fbaacad38614cab2c2701f
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-09 07:50:56 -0600

    advent rucksack reorganization, working on input

---
+++ code_challenges/advent_of_code/rucksack_reorganization.py
@@ -0,0 +1,9 @@
+def rucksack_reorganization():
+    supplies = open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/rucksack_input")
+    item = supplies.read().split("\n")
+    supplies.close()
+    
+    with open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/rucksack_input") as supplies:
+        item = supplies.read().split("\n")
+
+    return item
