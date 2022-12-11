5d081799a7aea0bc055383156902a1fc00967aae
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-11 13:20:50 -0600

    advent rucksack reorganization

--- code_challenges/advent_of_code/rucksack_reorganization.py
+++ code_challenges/advent_of_code/rucksack_reorganization.py
@@ -1,9 +1,30 @@
 def rucksack_reorganization():
     supplies = open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/rucksack_input")
-    item = supplies.read().split("\n")
+    items = supplies.read().split("\n")
     supplies.close()
     
     with open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/rucksack_input") as supplies:
-        item = supplies.read().split("\n")
+        items = supplies.read().split("\n")
 
-    return item
+def find_common_character(items):
+    common_characters = []
+    for item in items:
+        length = len(item)
+        half_way = length // 2
+        part_1 = item[0:half_way]
+        part_2 = item[half_way:]
+        for character in part_1:
+            if character in part_2:
+                common_characters.append(character)
+
+    return common_characters
+
+def find_priority():
+    common_characters = find_common_character()
+    print(common_characters)
+
+
+    # return 
+
+print(rucksack_reorganization())
+# print(find_common_character())
