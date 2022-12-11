a8b6b713b2674441b183a3c4393cf3c69f7f0c32
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-11 13:34:33 -0600

    CW finding arrows

--- code_challenges/code_wars/finding_arrows.py
+++ code_challenges/code_wars/finding_arrows.py
@@ -2,10 +2,11 @@ def finding_arrows(characters):
     points = 0
     total_points = 0
     for i in range(len(characters)):
-        print(characters[i], points)
-        if characters[i] == ">":
+        print(characters[i], total_points)
+        if characters[i] == ">" and characters[i - 1] == "-" or characters[i -1] == "=":
             total_points += 1
         elif characters[i] == "-":
+            total_points += 1
             
             
         
