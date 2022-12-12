2d2d2c7b46344fe55d0972e891e69b59d5ea2b71
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-12 10:00:01 -0600

    advent rucksack reorganization

--- code_challenges/advent_of_code/rucksack_reorganization.py
+++ code_challenges/advent_of_code/rucksack_reorganization.py
@@ -6,25 +6,89 @@ def rucksack_reorganization():
     with open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/rucksack_input") as supplies:
         items = supplies.read().split("\n")
 
-def find_common_character(items):
-    common_characters = []
+    return items
+
+def find_common_character():
+    items = rucksack_reorganization()
+    first_half = []
+    second_half = []
+    # common_character = []
     for item in items:
         length = len(item)
         half_way = length // 2
-        part_1 = item[0:half_way]
-        part_2 = item[half_way:]
-        for character in part_1:
-            if character in part_2:
-                common_characters.append(character)
+        # first_half.append(item[0:half_way])
+        # second_half.append(item[half_way:])
+        first_half = item[0:half_way]
+        second_half = item[half_way:]
+        
+    
+    
+    
+    
+    
+        
+    return common_character
 
-    return common_characters
+# def find_priority():
+#     priority_level = {
+#         "a": 1,
+#         "b": 2,
+#         "c": 3,
+#         "d": 4,
+#         "e": 5,
+#         "f": 6,
+#         "g": 7,
+#         "h": 8,
+#         "i": 9,
+#         "j": 10,
+#         "k": 11,
+#         "l": 12,
+#         "m": 13,
+#         "n": 14,
+#         "o": 15,
+#         "p": 16,
+#         "q": 17,
+#         "r": 18,
+#         "s": 19,
+#         "t": 20,
+#         "u": 21,
+#         "v": 22,
+#         "w": 23,
+#         "x": 24,
+#         "y": 25,
+#         "z": 26,
+#         "A": 27,
+#         "B": 28,
+#         "C": 29,
+#         "D": 30,
+#         "E": 31,
+#         "F": 32,
+#         "G": 33,
+#         "H": 34,
+#         "I": 35,
+#         "J": 36,
+#         "K": 37,
+#         "L": 38,
+#         "M": 39,
+#         "N": 40,
+#         "O": 41,
+#         "P": 42,
+#         "Q": 43,
+#         "R": 44,
+#         "S": 45,
+#         "T": 46,
+#         "U": 47,
+#         "V": 48,
+#         "W": 49,
+#         "X": 50,
+#         "Y": 51,
+#         "Z": 52,
+#     }
+#     common_characters = find_common_character()
+#     priority_level_amount = 0
+#     for character in common_characters:
+#         priority_level_amount += priority_level[character]
+#     return common_characters, priority_level_amount
 
-def find_priority():
-    common_characters = find_common_character()
-    print(common_characters)
 
-
-    # return 
-
-print(rucksack_reorganization())
-# print(find_common_character())
+print(find_common_character())
