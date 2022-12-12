39acb3eb7535bed83c62b5f505321728ff8531ee
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-12 15:06:01 -0600

    advent calorie counting complete

--- code_challenges/advent_of_code/calorie_counting.py
+++ code_challenges/advent_of_code/calorie_counting.py
@@ -1,10 +1,14 @@
-def get_elves_calories():
-    calories = open("/Users/amelia/projects/code-challenges/code_challenges/adhoc/calorie_counting_input")
-    elves_calories = calories.read().split("\n\n")
-    calories.close()
-
-    with open("/Users/amelia/projects/code-challenges/code_challenges/adhoc/calorie_counting_input") as calories:
+def elves_input():
+    with open(
+        "/Users/amelia/projects/code-challenges/code_challenges/adhoc/calorie_counting_input"
+    ) as calories:
         elves_calories = calories.read().split("\n\n")
+    return elves_calories
+
+
+def get_elves_calories():
+
+    elves_calories = elves_input()
 
     highest_elf_calorie = 0
     elves_calories = [elf_calorie.replace("\n", ",") for elf_calorie in elves_calories]
@@ -15,12 +19,4 @@ def get_elves_calories():
         if elf_calorie > highest_elf_calorie:
             highest_elf_calorie = elf_calorie
 
-
-
-#Test with mocking calls?
-
     return highest_elf_calorie
-print(get_elves_calories())
-# if __name__ == "__main__":
-#     elves_calories = ['1000,2000,3000', '4000', '5000,6000', '7000,8000,9000', '10000']
-#     print(get_elves_calories(elves_calories))
--- code_challenges/advent_of_code/calorie_counting_pt2.py
+++ code_challenges/advent_of_code/calorie_counting_pt2.py
@@ -1,10 +1,14 @@
-def get_elves_calories_top_3():
-    calories = open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/calorie_counting_input")
-    elves_calories = calories.read().split("\n\n")
-    calories.close()
-
-    with open("/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/calorie_counting_input") as calories:
+def elves_input():
+    with open(
+        "/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/calorie_counting_input"
+    ) as calories:
         elves_calories = calories.read().split("\n\n")
+
+    return elves_calories
+
+
+def get_elves_calories_top_3():
+    elves_calories = elves_input()
 
     highest_elf_calorie_1 = 0
     highest_elf_calorie_2 = 0
@@ -24,11 +28,4 @@ def get_elves_calories_top_3():
         elif elf_calorie > highest_elf_calorie_1:
             highest_elf_calorie_1 = elf_calorie
 
-
     return highest_elf_calorie_1 + highest_elf_calorie_2 + highest_elf_calorie_3
-print(get_elves_calories_top_3())
-
-
-# if __name__ == "__main__":
-#     elves_calories = ['1000,2000,3000', '4000', '5000,6000', '7000,8000,9000', '10000']
-#     print(get_elves_calories_top_3(elves_calories))
