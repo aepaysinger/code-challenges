6ba70aa11f903afc6e5062b7c6aff21b282e3890
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-19 07:23:43 -0600

    CW popping balloons, failing codewars test

---
+++ code_challenges/code_wars/popping_balloons.py
@@ -0,0 +1,94 @@
+class Balloons:
+    def __init__(self, amount, balloons):
+        self.balloons = balloons
+        self.amount = amount
+        self.balloons_amount = {}
+        self.popped_ballons = []
+
+    
+    def track_balloons(self):
+        for balloon in self.balloons:
+            self.balloons_amount[balloon] = self.balloons_amount.get(balloon, 0) + 1
+        return self.balloons_amount
+
+    def find_top_balloons(self):
+        top_amount = 0
+        second_amount = 0
+        top_balloon = None
+        second_balloon = None
+        
+        for balloon in self.balloons_amount:
+            if self.balloons_amount[balloon] > top_amount:
+                top_amount = self.balloons_amount[balloon]
+                top_balloon = balloon
+            elif self.balloons_amount[balloon] > second_amount and self.balloons_amount[balloon] <= top_amount:
+                second_amount = self.balloons_amount[balloon]
+                second_balloon = balloon
+        return top_balloon, top_amount, second_balloon, second_amount
+
+    def pop_most(self):
+        top_balloon, top_amount, second_balloon, second_amount = self.find_top_balloons()
+        one_left = all(x == 1 for x in self.balloons_amount.values())
+        if one_left:
+            while len(self.popped_ballons) < self.amount:
+                self.popped_ballons.append(self.balloons[-1])
+                self.balloons.pop(-1)
+                return self.popped_ballons
+        
+        if top_amount > second_amount:
+                self.balloons_amount[top_balloon] -= 1
+                for i in range(len(self.balloons) -1, -1, -1):
+                    if self.balloons[i] == top_balloon:
+                        self.popped_ballons.append(self.balloons[i])
+                        self.balloons.pop(i)
+                        break
+        elif top_amount == second_amount:
+            the_biggest = None
+            for i in range(len(self.balloons) -1, -1, -1):
+                if self.balloons[i] == top_balloon or self.balloons[i] == second_balloon:
+                    the_biggest = self.balloons[i]
+                    self.balloons_amount[the_biggest] -= 1
+                    break
+
+            for i in range(len(self.balloons) -1, -1, -1):
+                if self.balloons[i] == the_biggest:
+                    self.popped_ballons.append(self.balloons[i])
+                    self.balloons.pop(i)    
+                    break
+
+ 
+        return self.balloons, self.popped_ballons
+
+    def pop_most_amount(self):
+        for i in range(self.amount):
+            self.pop_most()
+        return self.popped_ballons
+
+def freq_stack(pops, balloons):
+    balloons = Balloons(pops, balloons)
+    balloons.track_balloons()
+    balloons.find_top_balloons()
+    
+    return balloons.pop_most_amount()
+
+
+if __name__ == "__main__":
+       pops = 10
+       balloons = [1, 2, 3, 4, 5, 5, 7, 8, 9, 10]
+       print(freq_stack(pops, balloons))
+
+# 7, [12, 9, 8, 8, 12, 12, 6, 1, 8]
+# [8, 12, 12, 8, 1, 6, 8]
+
+
+# balloons = Balloons([5, 7, 5, 7, 4, 5], 4)
+# balloons.track_balloons()
+# balloons.find_top_balloons()
+# print(balloons.pop_most_amount())
+
+# print(balloons.pop_most())
+# print(balloons.pop_most())
+# print(balloons.pop_most())
+# print(balloons.pop_most())
+# print(balloons.pop_most())
+# print(balloons.popped_ballons)
---
+++ tests/code_wars/test_popping_balloons.py
@@ -0,0 +1,58 @@
+from code_challenges.code_wars.popping_balloons import Balloons, freq_stack
+
+
+def test_popping_balloons_4_pops():
+    actual = freq_stack(4, [5, 7, 5, 7, 4, 5])
+    expected = [5, 7, 5, 4]
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_popping_balloons_10_pops():
+    actual = freq_stack(10, [1, 2, 3, 4, 5, 5, 7, 8, 9, 10])
+    expected = [5, 10, 9, 8, 7, 5, 4, 3, 2, 1]
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_popping_balloons_5_pops():
+    actual = freq_stack(5, [1, 1, 1, 10, 5, 5, 10, 7, 8])
+    expected = [1, 10, 5, 1, 8]
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_popping_balloons_7_pops():
+    actual = freq_stack(7, [12, 9, 8, 8, 12, 12, 6, 1, 8])
+    expected = [8, 12, 12, 8, 1, 6, 8]
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_track_balloons():
+    balloons = Balloons(4, [5, 7, 5, 7, 4, 5])
+    
+    assert balloons.track_balloons() == {5: 3, 7: 2, 4: 1}
+
+
+def test_find_top_balloon():
+    balloons = Balloons(4, [5, 7, 5, 7, 4, 5])
+    balloons.track_balloons()
+    
+    assert balloons.find_top_balloons() == (5, 3, 7, 2)
+
+
+def test_pop_most():
+    balloons = Balloons(4, [5, 7, 5, 7, 4, 5])
+    balloons.track_balloons()
+    balloons.find_top_balloons()
+    balloons.pop_most()
+
+    assert balloons.popped_ballons == [5]
+    assert balloons.balloons_amount == {5: 2, 7: 2, 4: 1}
+
+    balloons.pop_most()
+
+    assert balloons.popped_ballons == [5, 7]
+    assert balloons.balloons_amount == {5: 2, 7: 1, 4: 1}
+
