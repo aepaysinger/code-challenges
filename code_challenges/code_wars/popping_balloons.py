0283d800cf2fc93e01cf0fde5eea46ee2939ae93
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-20 07:06:09 -0600

    CW popping-ballons, made changes, still need to figure input

--- code_challenges/code_wars/popping_balloons.py
+++ code_challenges/code_wars/popping_balloons.py
@@ -1,7 +1,6 @@
 class Balloons:
-    def __init__(self, amount, balloons):
+    def __init__(self, balloons):
         self.balloons = balloons
-        self.amount = amount
         self.balloons_amount = {}
         self.popped_ballons = []
 
@@ -28,57 +27,55 @@ class Balloons:
                 second_balloon = balloon
         return top_balloon, top_amount, second_balloon, second_amount
 
-    def pop_most(self):
-        top_balloon, top_amount, second_balloon, second_amount = self.find_top_balloons()
-        one_left = all(x == 1 for x in self.balloons_amount.values())
-        if one_left:
-            while len(self.popped_ballons) < self.amount:
-                self.popped_ballons.append(self.balloons[-1])
-                self.balloons.pop(-1)
-                return self.popped_ballons
+    def pop_most(self, amount):
         
-        if top_amount > second_amount:
-                self.balloons_amount[top_balloon] -= 1
+        for _ in range(amount):
+            top_balloon, top_amount, second_balloon, second_amount = self.find_top_balloons()
+            one_left = all(x == 1 for x in self.balloons_amount.values())
+            if one_left:
+                self.popped_ballons.append(self.balloons.pop(-1))
+                continue
+            
+            if top_amount > second_amount:
+                    self.balloons_amount[top_balloon] -= 1
+                    for i in range(len(self.balloons) -1, -1, -1):
+                        if self.balloons[i] == top_balloon:
+                            self.popped_ballons.append(self.balloons[i])
+                            self.balloons.pop(i)
+                            break
+            elif top_amount == second_amount:
+                the_biggest = None
                 for i in range(len(self.balloons) -1, -1, -1):
-                    if self.balloons[i] == top_balloon:
-                        self.popped_ballons.append(self.balloons[i])
-                        self.balloons.pop(i)
+                    if self.balloons[i] == top_balloon or self.balloons[i] == second_balloon:
+                        the_biggest = self.balloons[i]
+                        self.balloons_amount[the_biggest] -= 1
                         break
-        elif top_amount == second_amount:
-            the_biggest = None
-            for i in range(len(self.balloons) -1, -1, -1):
-                if self.balloons[i] == top_balloon or self.balloons[i] == second_balloon:
-                    the_biggest = self.balloons[i]
-                    self.balloons_amount[the_biggest] -= 1
-                    break
 
-            for i in range(len(self.balloons) -1, -1, -1):
-                if self.balloons[i] == the_biggest:
-                    self.popped_ballons.append(self.balloons[i])
-                    self.balloons.pop(i)    
-                    break
+                for i in range(len(self.balloons) -1, -1, -1):
+                    if self.balloons[i] == the_biggest:
+                        self.popped_ballons.append(self.balloons[i])
+                        self.balloons.pop(i)    
+                        break
 
  
-        return self.balloons, self.popped_ballons
-
-    def pop_most_amount(self):
-        for i in range(self.amount):
-            self.pop_most()
         return self.popped_ballons
 
+
 def freq_stack(pops, balloons):
-    balloons = Balloons(pops, balloons)
+    balloons = Balloons(balloons)
     balloons.track_balloons()
     balloons.find_top_balloons()
     
-    return balloons.pop_most_amount()
+    return balloons.pop_most(pops)
+    
+    
 
 
 
 
 if __name__ == "__main__":
-       pops = 10
-       balloons = [1, 2, 3, 4, 5, 5, 7, 8, 9, 10]
+       pops = 7
+       balloons = [13, 4, 6, 13, 6, 9, 13]
        print(freq_stack(pops, balloons))
 
 # 7, [12, 9, 8, 8, 12, 12, 6, 1, 8]
