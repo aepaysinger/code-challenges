b9b4df317e9b39b57b306783633cf70940f6996e
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-21 14:02:15 -0600

    advent device space

--- code_challenges/advent_of_code/device_space.py
+++ code_challenges/advent_of_code/device_space.py
@@ -1,4 +1,4 @@
-def device_code():
+def code_device():
     with open(
         "/Users/amelia/projects/code-challenges/code_challenges/advent_of_code/device_input"
     ) as code:
@@ -8,11 +8,26 @@ def device_code():
 
 
 def build_directories():
-    device_code = device_code()
-    computer_directories = {}
+    device_code = code_device()
+    computer_directories = {"/": None}
+    directory_name = ""
     for i, line in enumerate(device_code):
-        if len(line) == 2:
-            if line[0] == "ls":
+        if len(line) == 3:
+            if line[1] == "cd":
+                directory_name = line[2]
+        elif len(line) == 2 and line[0] == "dir":
+            # computer_directories[directory_name].update({line[1]: []})
+            computer_directories[directory_name] = {line[1]: []}
+            directory_name = {line[1]}
+        elif len(line) == 2 and line[1] == "ls":
+            continue
+        elif len(line) == 2:
+            continue
+            # computer_directories[directory_name] = computer_directories.get(line[1])
+
+    return computer_directories
+
+
                 
                 
-print(device_code())
+print(build_directories())
