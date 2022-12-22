ecac4a2de585518ad121b0bae90fcff9600e66d5
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-22 13:22:12 -0600

    advent device space

--- code_challenges/advent_of_code/device_space.py
+++ code_challenges/advent_of_code/device_space.py
@@ -9,46 +9,55 @@ def code_device():
 
 def build_directories():
     device_code = code_device()
-    computer_directories = {"/": {}}
-    directory_name = [] # [/]
-    directory_level = 0
+    computer_directories = {"/": {}} # {"/": {"a": [["b.txt", "14848514"], ["c.dat": "8504156"], {"d": []}}
+    directory_name = [] # [/,]
+    directory_level = 0 # 0 
     for i, line in enumerate(device_code):
+        print(line)
+        print(computer_directories)
+        print(directory_name, directory_level)
         if len(line) == 3 and line[1] == "cd":
-            directory_name.append(line[2])
+            if line[2] not in directory_name:
+                directory_name.append(line[2])
+            directory_level = directory_name.index(line[2])
         elif len(line) == 2 and line[1] == "ls":
             continue
-        elif len(line) == 2 and line[0] == dir:
-            computer_directories[directory_name[directory_level]].update(line[1], [])
+        elif len(line) == 2 and line[0] == "dir":
+            computer_directories[directory_name[directory_level]].update({line[1]: []})
             directory_name.append(line[1])
         elif len(line) == 2:
-            computer_directories[old_directory_name][new_directory_name].append([line[0], line[1]])
+            computer_directories[directory_name[directory_level]][directory_name[-1]].append([line[1], line[0]])
 
 
     return computer_directories
-
-$ cd / 
-$ ls (skipped)
-dir a 
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
+{"/": {
+    a: ["b.txt": 148], ["c.dat, 850"], {
+        e: 
+    } 
+    d: []}}
+# $ cd /
+# $ ls
+# dir a
+# 14848514 b.txt
+# 8504156 c.dat
+# dir d
+# $ cd a
+# $ ls
+#  dir e
+# 29116 f
+# 2557 g
+# 62596 h.lst
+# $ cd e
+# $ ls
+# 584 i
+# $ cd ..
+# $ cd ..
+# $ cd d
+# $ ls
+# 4060174 j
+# 8033020 d.log
+# 5626152 d.ext
+# 7214296 k
                 
                 
 print(build_directories())
