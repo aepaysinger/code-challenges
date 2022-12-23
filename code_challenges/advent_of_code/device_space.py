e5a84e5714b36fc48fa61db96b1d5c47402fb1b2
Amelia Paysinger <ameliapaysinger@gmail.com>
2022-12-23 09:58:55 -0600

    advent device space

--- code_challenges/advent_of_code/device_space.py
+++ code_challenges/advent_of_code/device_space.py
@@ -9,28 +9,29 @@ def code_device():
 
 def build_directories():
     device_code = code_device()
-    computer_directories = {"/": {}} # 
-    directory_name = [] # 
-    directory_level = 0 # 
+    computer_directories = {"/": {}} # {"/": "a": {"e": {"i": 584}, "f": 29116, "g": 2557, "h.lst": 62596}, "b.txt": 14848514, "c.dat": 8504156, "d": {"j": 4060174}}
+    directory_name = [] # /, a, d, e
+    directory_level = 0 # 0, 1, 3, 2, 1, 2
     for i, line in enumerate(device_code):
-        print(line)
         print(computer_directories)
-        print(directory_name, directory_level)
         if len(line) == 3 and line[1] == "cd":
+            if line[2] == "..":
+                directory_level -= 1
+                continue
             if line[2] not in directory_name:
                 directory_name.append(line[2])
             directory_level = directory_name.index(line[2])
         elif len(line) == 2 and line[1] == "ls":
             continue
         elif len(line) == 2 and line[0] == "dir":
-            computer_directories[directory_name[directory_level]].update({line[1]: []})
+            computer_directories[directory_name[directory_level]].update({line[1]: {}})
             directory_name.append(line[1])
         elif len(line) == 2:
-            computer_directories[directory_name[directory_level]][directory_name[-1]].append([line[1], line[0]])
+            computer_directories[directory_name[directory_level]].update({line[1]: int(line[0])})
 
 
     return computer_directories
-{"/": {"a": {"e": {}}, "b.txt": 148, "c.dat": 850, "d": {}}
+
 
 # $ cd /
 # $ ls
