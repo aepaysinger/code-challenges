c619f4350ef8ef9b6b4e28fb49914d914e55db17
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-01-02 09:58:44 -0600

    adhock challenge, removed stray comments, ran black

--- code_challenges/adhoc/ad_hock_challenge.py
+++ code_challenges/adhoc/ad_hock_challenge.py
@@ -1,18 +1,18 @@
-#Write a function that takes a dictionary, string, and iterable of values.
+# Write a function that takes a dictionary, string, and iterable of values.
 # The dictionaries keys will be strings and the values will be functions.
-# The return value should be the return value from the function from the input dict,  at the input string, 
+# The return value should be the return value from the function from the input dict,  at the input string,
 # when called with the values in the input iterable as arguments.
 def find_the_function(functions, function_name, values):
-    # calling = functions[function_name]
-    return functions[function_name](*values)#calling(the_numbers)
-    
+
+    return functions[function_name](*values)
+
+
 def adding(*nums):
-    
-      
+
     return sum(nums)
 
 
-def even_it_up(*the_numbers): # *mutiple arguments
+def even_it_up(*the_numbers):
     evens = []
     for number in the_numbers:
         if number % 2 == 0:
@@ -24,7 +24,6 @@ if __name__ == "__main__":
     functions = {
         "adding": adding,
         "even_it_up": even_it_up,
-
     }
     function_name = "adding"
     the_numbers = [10, 13, 20]
