1b7b27058367ddebc77e3c0d2ad1e0131195eae5
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-01-08 13:26:20 -0600

    CW robot path, updated code - put in while loop, moved out block

--- code_challenges/code_wars/robot_path.py
+++ code_challenges/code_wars/robot_path.py
@@ -1,50 +1,48 @@
 class RobotDirections:
     def __init__(self, directions):
         self.directions = directions
-
-    def translate_directions(self):
-        translated_instructions = []
-        direction_translation = {
+        self.direction_translation = {
             "<": "left",
             ">": "right",
             "^": "up",
             "v": "down",
         }
+
+    def translate_directions(self):
+        translated_instructions = []
+
         i = 0
         j = i + 1
         if self.directions == "":
             return "Paused"
 
-        for _ in range(len(self.directions) + 1):
-            if j == len(self.directions):
+        while j != len(self.directions):
+            for _ in range(len(self.directions) + 1):
+                if j == len(self.directions):
+                    if len(self.directions[i:j]) == 1:
+                        translated_instructions.append(
+                            f"Take 1 step {self.direction_translation[self.directions[i]]}"
+                        )
+                        return "\n".join(translated_instructions)
+                    else:
+                        translated_instructions.append(
+                            f"Take {len(self.directions[i:j])} steps {self.direction_translation[self.directions[i]]}"
+                        )
+                        return "\n".join(translated_instructions)
+                while self.directions[i] == self.directions[j]:
+                    j += 1
+                    if j == len(self.directions):
+                        break
                 if len(self.directions[i:j]) == 1:
                     translated_instructions.append(
-                        f"Take 1 step {direction_translation[self.directions[i]]}"
+                        f"Take 1 step {self.direction_translation[self.directions[i]]}"
                     )
-                    return "\n".join(translated_instructions)
                 else:
                     translated_instructions.append(
-                        f"Take {len(self.directions[i:j])} steps {direction_translation[self.directions[i]]}"
+                        f"Take {len(self.directions[i:j])} steps {self.direction_translation[self.directions[i]]}"
                     )
-                    return "\n".join(translated_instructions)
-            while self.directions[i] == self.directions[j]:
-                j += 1
-                if j == len(self.directions):
-                    break
-            if len(self.directions[i:j]) == 1:
-                translated_instructions.append(
-                    f"Take 1 step {direction_translation[self.directions[i]]}"
-                )
-                if j == len(self.directions):
-                    break
-                i = j
-                j = i + 1
-            else:
-                translated_instructions.append(
-                    f"Take {len(self.directions[i:j])} steps {direction_translation[self.directions[i]]}"
-                )
-                if j == len(self.directions):
-                    break
+                    if j == len(self.directions):
+                        break
                 i = j
                 j = i + 1
         return "\n".join(translated_instructions)
