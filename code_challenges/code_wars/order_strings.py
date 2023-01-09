5076b613d9953b5d11f7ebf9fe3e721f06189ec4
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-01-09 15:29:50 -0600

    CW order strings, changed code and updated tests to match

--- code_challenges/code_wars/order_strings.py
+++ code_challenges/code_wars/order_strings.py
@@ -2,35 +2,43 @@ class OrderStrings:
     def __init__(self, text):
         self.text = text
         self.split_text = text.split(" ")
-        self.position_in_string = {}
 
-    def find_the_number(self):
-        position = 0
-        for item in self.split_text:
-            for i in range(len(item)):
-                if item[i].isdigit():
-                    self.position_in_string[position] = int(item[i]) - 1
-                    position += 1
-        return self.position_in_string
+    def sort_text(self):
+        return " ".join(sorted(self.split_text, key=self.find_position))
 
-    def build_updated_order_string(self):
-        placement = 0
-        ordered_string = ""
-        while len(ordered_string) < len(self.text):
-            for key, value in self.position_in_string.items():
-                if value == placement:
-                    ordered_string += self.split_text[key] + " "
+    def find_position(self, characters):
+        for character in characters:
+            if character.isdigit():
+                return int(character)
 
-                    placement += 1
+    #     self.position_in_string = {}
 
-        return ordered_string[:-1]
+    # def find_the_number(self):
+    #     position = 0
+    #     for item in self.split_text:
+    #         for i in range(len(item)):
+    #             if item[i].isdigit():
+    #                 self.position_in_string[position] = int(item[i]) - 1
+    #                 position += 1
+    #     return self.position_in_string
+
+    # def build_updated_order_string(self):
+    #     placement = 0
+    #     ordered_string = ""
+    #     while len(ordered_string) < len(self.text):
+    #         for key, value in self.position_in_string.items():
+    #             if value == placement:
+    #                 ordered_string += self.split_text[key] + " "
+
+    #                 placement += 1
+
+    #     return ordered_string[:-1]
 
 
 def order(text):
     changed_order = OrderStrings(text)
-    changed_order.find_the_number()
 
-    return changed_order.build_updated_order_string()
+    return changed_order.sort_text()
 
 
 if __name__ == "__main__":
