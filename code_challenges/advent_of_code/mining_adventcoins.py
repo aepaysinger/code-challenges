0abb73f4c0d911aa6c394f42ff6e3185db93b5ba
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-01-22 18:31:37 -0600

    advent minig  adventcoins

--- code_challenges/advent_of_code/mining_adventcoins.py
+++ code_challenges/advent_of_code/mining_adventcoins.py
@@ -1,23 +1,32 @@
 import hashlib
 
-def mining_adventcoins():
-    secret_key = "abcdef"
-    # final_code = ""
 
-    number = up_the_number()
-    code = secret_key + str(number)
-    code_bytes = bytes(code, 'utf-8')
-    m = hashlib.md5()
-    m.update(code_bytes)
-    m.digest()
-    final_code = m.hexdigest()
+class MiningAdventCoins:
+    def __init__(self, code):
+        self.code = code
+        self.number = 0
+        self.final_code = ""
 
-    print(number)    
-    print(final_code)
-def up_the_number():
-    number = 609041
+    def mining_adventcoins(self):
 
-print(mining_adventcoins())
+        while self.final_code[:6] != "00000":
+            code_number = self.up_the_number()
+            code_to_check = self.code + str(code_number)
+            code_bytes = bytes(code_to_check, 'utf-8')
+            m = hashlib.md5()
+            m.update(code_bytes)
+            m.digest()
+            self.final_code = m.hexdigest()
+        
+        return self.final_code, self.number
+            
+
+    def up_the_number(self):
+        self.number += 1
+        return self.number 
+
+mine_coins = MiningAdventCoins("abcdef")
+print(mine_coins.mining_adventcoins())
 # arr = bytes(string, 'utf-8')
 # arr2 = bytes(string, 'ascii')
 
