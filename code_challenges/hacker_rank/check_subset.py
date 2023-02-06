56eb89a4fe5b9221f47ad248f70b50a20aa49c91
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-06 06:53:30 -0800

    HR check subset

---
+++ code_challenges/hacker_rank/check_subset.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    number_of_test_cases = int(input())
+    for i in range(number_of_test_cases):
+        length_of_case_A = int(input())
+        case_A = input().split()
+        
+    print(number_of_test_cases)
+    print(length_of_case_A, case_A)
