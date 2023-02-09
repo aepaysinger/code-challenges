9498d99ee91f768ffa97458c7088387e673567c1
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-09 09:47:45 -0600

    CW pairs of shoes

--- code_challenges/code_wars/pairs_of_shoes.py
+++ code_challenges/code_wars/pairs_of_shoes.py
@@ -1,13 +1,29 @@
+from collections import Counter
+
+
 def make_pairs(mixed_shoes):
     shoes = {}
+    
     for foot, size in mixed_shoes:
-        shoes[size] = shoes.get(size, foot)
-    print(shoes)
+        if size not in shoes:
+            shoes[size] = {0: 0, 1: 0}
+            shoes[size][foot] += 1
+        else:
+            shoes[size][foot] +=1
+
+    return shoes
+
+def find_pairs(mixed_shoes):
+    shoes = make_pairs(mixed_shoes)
+    shoe_pairs = False
+    for size in shoes:
+        if shoes[size][0] == shoes[size][1]:
+            shoe_pairs = True
+    return shoe_pairs
 
 
 if __name__ == "__main__":
-    mixed_shoes = [[0, 21], 
-         [1, 23], 
-         [1, 21], 
-         [0, 23]]
-    print(make_pairs(mixed_shoes))
+    mixed_shoes = [[0, 23], [1, 23], [1, 23], [0, 21], [0, 23], [0, 23]]
+    print(find_pairs(mixed_shoes))
+
+    # use a counter --> collections counter
