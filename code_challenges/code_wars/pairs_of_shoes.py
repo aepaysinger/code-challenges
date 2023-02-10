05557279a23256b113cd6213a9fa5cde17dc7f22
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-10 09:16:12 -0600

    CW paris of shoes, complete

--- code_challenges/code_wars/pairs_of_shoes.py
+++ code_challenges/code_wars/pairs_of_shoes.py
@@ -1,17 +1,15 @@
-from collections import Counter
-
-
 def make_pairs(mixed_shoes):
     shoes = {}
-    
+
     for foot, size in mixed_shoes:
         if size not in shoes:
             shoes[size] = {0: 0, 1: 0}
             shoes[size][foot] += 1
         else:
-            shoes[size][foot] +=1
+            shoes[size][foot] += 1
 
     return shoes
+
 
 def find_pairs(mixed_shoes):
     shoes = make_pairs(mixed_shoes)
@@ -20,12 +18,25 @@ def find_pairs(mixed_shoes):
         if shoes[size][0] == shoes[size][1]:
             shoe_pairs = True
         else:
-            shoe_pairs = False
+            return False
     return shoe_pairs
 
 
 if __name__ == "__main__":
-    mixed_shoes = [[0, 21], [1, 23], [1, 21], [1, 23]]
-    print(make_pairs(mixed_shoes))
-
-    # use a counter --> collections counter
+    mixed_shoes = [
+        [0, 38],
+        [0, 31],
+        [1, 34],
+        [0, 47],
+        [0, 44],
+        [1, 47],
+        [1, 46],
+        [0, 37],
+        [0, 46],
+        [1, 31],
+        [1, 41],
+        [1, 38],
+        [0, 41],
+        [0, 34],
+    ]
+    print(find_pairs(mixed_shoes))
--- tests/code_wars/test_pairs_of_shoes.py
+++ tests/code_wars/test_pairs_of_shoes.py
@@ -3,11 +3,7 @@ from code_challenges.code_wars.pairs_of_shoes import make_pairs, find_pairs
 
 def test_make_pairs():
     actual = make_pairs([[0, 23], [1, 21], [1, 23], [0, 21], [1, 22], [0, 22]])
-    expected = {
-        23: {0: 1, 1: 1},
-        21: {0: 1, 1: 1},
-        22: {0: 1, 1: 1}
-    }
+    expected = {23: {0: 1, 1: 1}, 21: {0: 1, 1: 1}, 22: {0: 1, 1: 1}}
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
 
