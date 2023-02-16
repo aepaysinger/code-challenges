0f3ed5e428bf82629110429aa4a710a5b0552e0a
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-02-16 15:14:24 -0600

    leet two sum needs tests

---
+++ code_challenges/adhoc/two_sum.py
@@ -0,0 +1,16 @@
+def two_sum(numbers, target):
+
+    for i in range(len(numbers)):
+        for j in  range(1, len(numbers)): 
+            if i == j:
+                continue
+            if numbers[i] + numbers[j] == target:
+                return [i, j]
+        
+       
+if __name__ == "__main__":
+    target = 6
+    numbers = [3,2,4]
+    print(two_sum(numbers, target))
+
+## complete needs tests
