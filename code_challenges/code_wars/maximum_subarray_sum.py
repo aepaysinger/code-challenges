aa87a83c1182d9cd4549500258bec076a1e3b06c
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-13 10:53:10 -0500

    maximum subarray sum

--- code_challenges/code_wars/maximum_subarray_sum.py
+++ code_challenges/code_wars/maximum_subarray_sum.py
@@ -1,67 +1,17 @@
 def max_sequence(sequence):
+
     total = 0
-    for number in sequence:
-        if number > 0:
-            total += 1
-            break
-    if total == 0:
-        return total
-    
-    arrays_to_check = [(0, len(sequence)-1)]
-    stop = len(sequence)-1
-    
-    
-    j = 1
-    
-    for i in range(len(sequence)): 
-        while sequence[j] >= 0 and j != stop:
-            j += 1
-        if j == stop:
-            arrays_to_check.append((i,j))
-        else:
-            arrays_to_check.append((i, j-1))
 
-        
-        print(arrays_to_check)
-        print(sequence[i], sequence[j])
-
-    for arrays in arrays_to_check:
-        if sum(sequence[arrays[0]:arrays[1]]) > total:
-            total = sum(sequence[arrays[0]:arrays[1]])
-        
-     
-            
-        
-    # for start_stop in arrays_to_check:
-    #     if start_stop[1] == len(sequence) - 1:
-    #         if sum(sequence[start_stop[0]:start_stop[1]]) > total:
-    #             total = sum(sequence[start_stop[0]:start_stop[1]])
-    #     if sum(sequence[start_stop[0]:start_stop[1]+1]) > total:
-    #         total = sum(sequence[start_stop[0]:start_stop[1]+1])
-    return total, arrays_to_check
-dont move i until j has gone to the end of the list
+    for i in range(len(sequence)):
+        j = len(sequence)
+        if sequence[i] < 1:
+            continue
+        while j > i:
+            if sequence[j-1] > 0 and sum(sequence[i:j]) > total:
+                total = sum(sequence[i:j])
+            j -= 1
+    return total
 
 if __name__ == "__main__":
-    sequence = [-2, 1, -3, 4, -1, 2, 1, -5, 4] #(0,8) (0,2) (4,6) 
+    sequence = [-2, 1, -3, 4, -1, 2, 1, -5, 4] 
     print(max_sequence(sequence))
-    # i start or after neg
-    # j last item or before neg
-# def max_sequence(sequence):
-#     total = 0
-#     for number in sequence:
-#         if number > 0:
-#             total += 1
-#             break
-#     if total == 0:
-#         return total
-    
-    
-#     for i in range(len(sequence)):
-#         j = len(sequence)
-#         if sequence[i] < 1:
-#             continue
-#         while j > i:
-#             if sequence[j-1] > 0 and sum(sequence[i:j]) > total:
-#                 total = sum(sequence[i:j])
-#             j -= 1
-#     return total
--- tests/code_wars/test_maximum_subarray_sum.py
+++ tests/code_wars/test_maximum_subarray_sum.py
@@ -8,27 +8,27 @@ def test_max_sequence():
     assert actual == expected, f"Returned {actual} instead of {expected}."
 
 
-# def test_max_sequence_all_negative():
-#     actual = max_sequence([-2, -1, -3, -4, -1, -2, -1, -5, -4])
-#     expected = 0
+def test_max_sequence_all_negative():
+    actual = max_sequence([-2, -1, -3, -4, -1, -2, -1, -5, -4])
+    expected = 0
 
-#     assert actual == expected, f"Returned {actual} instead of {expected}."
+    assert actual == expected, f"Returned {actual} instead of {expected}."
 
 
-# def test_max_sequence_empty():
-#     actual = max_sequence([])
-#     expected = 0
+def test_max_sequence_empty():
+    actual = max_sequence([])
+    expected = 0
 
-#     assert actual == expected, f"Returned {actual} instead of {expected}."
+    assert actual == expected, f"Returned {actual} instead of {expected}."
 
-# def test_max_sequence_A():
-#     actual = max_sequence([7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43])
-#     expected = 155
+def test_max_sequence_A():
+    actual = max_sequence([7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43])
+    expected = 155
 
-#     assert actual == expected, f"Returned {actual} instead of {expected}."
+    assert actual == expected, f"Returned {actual} instead of {expected}."
 
-def test_max_sequence_B():
-    actual = max_sequence([1,2,3,-4,5,6,7])
-    expected = [(0,6),(0,2),(4,6)]
-    assert actual == expected, f"{actual} != {expected}"
+# def test_max_sequence_B():
+#     actual = max_sequence([1,2,3,-4,5,6,7])
+#     expected = [(0,6),(0,2),(4,6)]
+#     assert actual == expected, f"{actual} != {expected}"
     
