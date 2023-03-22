1d9c12de55dbde4ee7e553494266775e7f3e240b
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-22 07:18:55 -0500

    changed how i build the rope with cuts

--- code_challenges/code_wars/cut_rope.py
+++ code_challenges/code_wars/cut_rope.py
@@ -1,33 +1,36 @@
 def cut_rope(length, m, n):
     rope = ["-" for _ in range(length)]
     different_lengths = {}
-    rope_with_cuts = rope
+    rope_with_cuts = []
     count_m = 0
     count_n = 0
     for i, section in enumerate(rope):
-        if section == "-":
-            count_m += 1
-            count_n += 1
+        rope_with_cuts.append(("-"))
+        count_m += 1
+        count_n += 1
         if count_m == m:
-            rope_with_cuts.insert(i + 1, ".")
+            rope_with_cuts.append(".")
             count_m = 0
         if count_n == n:
-            rope_with_cuts.insert(i + 1, ".")
-            count_n = 0
-  
+            if (i + 1) % m == 0 and (i + 1) % n == 0:
+                count_n = 0
+            else:
+                rope_with_cuts.append(".")
+                count_n = 0
+
     length_count = 0
 
-    for i in range(len(rope)):
-        if i == (len(rope) - 1) and rope[i] == "-":
+    for i in range(len(rope_with_cuts)):
+        if i == (len(rope_with_cuts) - 1) and rope_with_cuts[i] == "-":
             length_count += 1
             different_lengths[str(length_count) + "cm"] = (
                 different_lengths.get(str(length_count) + "cm", 0) + 1
             )
-        elif rope[i] == "-":
+        elif rope_with_cuts[i] == "-":
             length_count += 1
-        elif rope[i] == "." and length_count == 0:
+        elif rope_with_cuts[i] == "." and length_count == 0:
             continue
-        elif rope[i] == ".":
+        elif rope_with_cuts[i] == ".":
             different_lengths[str(length_count) + "cm"] = (
                 different_lengths.get(str(length_count) + "cm", 0) + 1
             )
@@ -40,7 +43,7 @@ def cut_rope(length, m, n):
 
 
 if __name__ == "__main__":
-    length = 7
+    length = 10
     m = 2
     n = 3
     print(cut_rope(length, m, n))
