c1945471149601a4d71a589c35fcd9198264fe76
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-27 10:53:38 -0500

    CW coordinates validator complete

---
+++ code_challenges/code_wars/coordinates_validator.py
@@ -0,0 +1,20 @@
+def is_valid_coordinates(coordinates):
+    for character in coordinates:
+        if character.isalpha():
+            return False
+    try:
+        x_coordinate = float(coordinates.split(", ")[0])
+        y_coordinate = float(coordinates.split(", ")[1])
+        print(x_coordinate)
+    except:
+        return False
+    if len(list(coordinates.split(","))) > 2:
+        return False
+    if -90 <= x_coordinate <= 90.00 and -180 <= y_coordinate <= 180.00:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    coordinates = "1e1, 1e1"
+    print(is_valid_coordinates(coordinates))
