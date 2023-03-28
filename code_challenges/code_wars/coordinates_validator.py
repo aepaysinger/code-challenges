aeca9692bbbf9799a2a720b5b4c0ea7ce431c1a3
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-28 14:42:31 -0500

    only split once

--- code_challenges/code_wars/coordinates_validator.py
+++ code_challenges/code_wars/coordinates_validator.py
@@ -2,12 +2,14 @@ def is_valid_coordinates(coordinates):
     for character in coordinates:
         if character.isalpha():
             return False
+    x_y_coordinates = coordinates.split(", ")
     try:
-        x_coordinate = float(coordinates.split(", ")[0])
-        y_coordinate = float(coordinates.split(", ")[1])
+        x_coordinate = float(x_y_coordinates[0])
+        y_coordinate = float(x_y_coordinates[1])
     except:
         return False
-    if len(list(coordinates.split(","))) > 2:
+
+    if len(x_y_coordinates) > 2:
         return False
     if -90 <= x_coordinate <= 90.00 and -180 <= y_coordinate <= 180.00:
         return True
@@ -15,5 +17,5 @@ def is_valid_coordinates(coordinates):
 
 
 if __name__ == "__main__":
-    coordinates = "1e1, 1e1"
+    coordinates = "24.53525235, 23.45235"
     print(is_valid_coordinates(coordinates))
