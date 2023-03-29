1039ece153a23bfd91e15f3f39497a8490b81ee7
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-29 12:25:00 -0500

    updated to loop once

--- code_challenges/adhoc/two_sum.py
+++ code_challenges/adhoc/two_sum.py
@@ -1,14 +1,18 @@
 def two_sum(numbers, target):
-
-    for i in range(len(numbers)):
-        for j in range(1, len(numbers)):
-            if i == j:
-                continue
-            if numbers[i] + numbers[j] == target:
-                return [i, j]
+    j = len(numbers) - 1
+    numbers.sort()
+    for i in numbers:
+        while numbers[i] + numbers[j] > target:
+            j -= 1
+        if numbers[i] + numbers[j] < target:
+            continue
+        if numbers[i] + numbers[j] == target:
+            result = [i, j]
+            result.sort()
+            return result
 
 
 if __name__ == "__main__":
     target = 6
-    numbers = [3, 2, 4]
+    numbers = [3, 3, 1, 8, 7, 4]
     print(two_sum(numbers, target))
