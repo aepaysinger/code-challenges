7691117975f8189dcbcf92665ee51880c804ee54
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-03-29 14:21:29 -0500

    updated tests

--- tests/adhoc/test_two_sum.py
+++ tests/adhoc/test_two_sum.py
@@ -3,13 +3,13 @@ from code_challenges.adhoc.two_sum import two_sum
 
 def test_two_sum_a():
     actual = two_sum([3, 2, 4], 6)
-    expected = [0, 2]
+    expected = [1, 2]
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
 
 
 def test_two_sum_b():
     actual = two_sum([3, 3, 1, 8, 7, 4], 6)
-    expected = [1, 2]
+    expected = [0, 1]
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
