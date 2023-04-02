bc658dbd39bd8d32e1fe59ef737524a5a4754bee
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-04-02 13:33:18 -0500

    product of array

--- code_challenges/adhoc/product_of_array.py
+++ code_challenges/adhoc/product_of_array.py
@@ -1,32 +1,21 @@
-import math
-
 def product_of_array(array):
-    # product_array = []
-    # count = 0
-    # for _ in range(len(array)):
-    #     product = 1
-    #     for i in range(len(array)):
-    #         if i != count:
-    #             product *= array[i]
-    #     count += 1
-    #     product_array.append(product)
-    # return product_array
-    product_of_array = []
+    forward = [1] * len(array)
+    backwards = [1] * len(array)
+    product_of_array = [1] * len(array)
+
+    for i in range(1, len(array)):
+        forward[i] = forward[i - 1] * array[i - 1]
     
-    for i in range(len(array)):
-        # need_to_add = array.pop(0)
-        # product_of_array.append(math.prod(array))
-        # array.append(need_to_add)
-        if i == 0:
-            product_of_array.append(math.prod(array[1:]))
-        else:
-        
-            product_of_array.append(math.prod(array[:i] + array[i+1:]))
-        
+    for i in range(len(array) - 2, -1, -1):
+        backwards[i] = backwards[i + 1] * array[i + 1]
+
+    for i in range(len(product_of_array)):
+        product_of_array[i] = forward[i] * backwards[i]
+
     return product_of_array
 
 
 
 if __name__ == "__main__":
     array = [1,2,3,4]
-    print(product_of_array(array))
+    print(product_of_array(array)) [24, 12, 8, 6]
