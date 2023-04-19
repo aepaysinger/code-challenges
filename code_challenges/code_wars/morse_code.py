cdb392291cbdd072db978d4b597d8eb3c2be7ced
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-04-19 09:59:15 -0500

    figuring out how to use strip()

--- code_challenges/code_wars/morse_code.py
+++ code_challenges/code_wars/morse_code.py
@@ -60,16 +60,27 @@ def decode_morse(morse_code):
     translation = ""
     morse_code = morse_code.split(" ")
 
-    for i in range(len(morse_code) - 1, -1, -1):
-        if morse_code[i] == "":
-            continue
-        else:
-            morse_code = morse_code[: i + 1]
-            break
-
-    for i, character in enumerate(morse_code):
-        if character == "" and morse_code[i + 1] == "" and len(translation) > 0:
+    # for i in range(len(morse_code) - 1, -1, -1):
+    #     if morse_code[i] == "":
+    #         continue
+    #     else:
+    #         morse_code = morse_code[: i + 1]
+    #         break
+    print(morse_code)
+    # for i, character in enumerate(morse_code):
+    #     if character == "" and morse_code[i + 1] == "" and len(translation) > 0:
+    #         translation += " "
+    #     else:
+    #         translation += translator[character]
+    # return translation.strip()
+    for i in range(len(morse_code) + 1):
+        if morse_code[i] == "" and morse_code[i + 1] == "" and len(translation) > 0:
             translation += " "
         else:
-            translation += translator[character]
-    return translation
+            translation += translator[morse_code[i]]
+    return translation.strip()
+
+
+if __name__ == "__main__":
+    morse_code = "      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.- "
+    print(decode_morse(morse_code))
