71be2bf2d7cad630973c1817fcdc092208b2d8e8
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-05-30 11:29:51 -0500

    advent giant squid, first part complete

--- code_challenges/advent_of_code/giant_squid.py
+++ code_challenges/advent_of_code/giant_squid.py
@@ -23,17 +23,44 @@ def get_bingo_info():
 
 def play_bingo():
     boards, numbers_to_call = get_bingo_info()
-    boards_with_number_called = {}
+    
+    
     for number_to_call in numbers_to_call:
         for board in boards:
-            for numbers in boards[board]:
-                for number in numbers:
-                    if number == number_to_call:
+            for row_number, rows in enumerate(boards[board]):
+                for i, number in enumerate(rows):
+                    if number_to_call == number:
+                        boards[board][row_number][i] = "X"
+                    if boards[board][row_number] == ["X", "X", "X", "X", "X"]:
+                        return boards[board], number
+                    if boards[board][0][0] == "X" and boards[board][1][0] == "X" and boards[board][2][0] == "X" and boards[board][3][0] == "X" and boards[board][4][0] == "X":
+                        return boards[board], number
+                    
+
+def find_winner_score():
+    wining_board, winning_number = play_bingo()
+    score = 0
+    for rows in wining_board:
+        for character in rows:
+            if character == "X":
+                continue
+            score += character
+    
+    return score * winning_number
+
+                   
+
+
+
+
+                        
+                        
                         
                         
             
 
-    return boards
+    # return boards
 
 # print(get_bingo_info())
-print(play_bingo())
+# print(play_bingo())
+print(find_winner_score())
