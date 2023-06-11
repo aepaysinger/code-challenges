a4cdb770db5979043f7ddede6bf0a5d359ae0fe0
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-06-11 15:29:11 -0500

    pulled out code into seperate function, added test

--- code_challenges/advent_of_code/treachery_of_whales.py
+++ code_challenges/advent_of_code/treachery_of_whales.py
@@ -9,12 +9,26 @@ def fuel_spent_to_align_position():
     crab_positions = get_crab_positions()
     middle_point = crab_positions[len(crab_positions) // 2]
     total_fuel = 0
-    for point in range(len(crab_positions)):
-        if crab_positions[point] > middle_point:
-            total_fuel += crab_positions[point] - middle_point
-        elif crab_positions[point] < middle_point:
-            total_fuel += middle_point - crab_positions[point]
+    for crab_position in crab_positions:
+        if crab_position > middle_point:
+            total_fuel += crab_position - middle_point
+        elif crab_position < middle_point:
+            total_fuel += middle_point - crab_position
 
+    return total_fuel
+
+
+def find_fuel_total(crab_positions, align_position):
+    total_fuel = 0
+    for crab_position in crab_positions:
+        if crab_position > align_position:
+            location_total = crab_position - align_position
+            for i in range(1, location_total + 1):
+                total_fuel += i
+        elif crab_position < align_position:
+            location_total = align_position - crab_position
+            for i in range(1, location_total + 1):
+                total_fuel += i
     return total_fuel
 
 
@@ -22,28 +36,10 @@ def fuel_spent_to_align_position_part2():
     crab_positions = get_crab_positions()
     align_position = round(sum(crab_positions) / len(crab_positions))
     align_position_b = sum(crab_positions) // len(crab_positions)
-    total_fuel = 0
-    total_fuel_b = 0
-    for point in range(len(crab_positions)):
-        if crab_positions[point] > align_position:
-            location_total = crab_positions[point] - align_position
-            for i in range(1, location_total + 1):
-                total_fuel += i
-        elif crab_positions[point] < align_position:
-            location_total = align_position - crab_positions[point]
-            for i in range(1, location_total + 1):
-                total_fuel += i
-    for point in range(len(crab_positions)):
-        if crab_positions[point] > align_position_b:
-            location_total = crab_positions[point] - align_position_b
-            for i in range(1, location_total + 1):
-                total_fuel_b += i
-        elif crab_positions[point] < align_position_b:
-            location_total = align_position_b - crab_positions[point]
-            for i in range(1, location_total + 1):
-                total_fuel_b += i
+    total_fuel = find_fuel_total(crab_positions, align_position)
+    total_fuel_b = find_fuel_total(crab_positions, align_position_b)
+
     if total_fuel < total_fuel_b:
         return total_fuel
     else:
         return total_fuel_b
-
--- tests/advent_of_code/test_treachery_of_whales.py
+++ tests/advent_of_code/test_treachery_of_whales.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from code_challenges.advent_of_code.treachery_of_whales import (
     fuel_spent_to_align_position,
     fuel_spent_to_align_position_part2,
+    find_fuel_total,
 )
 
 
@@ -29,5 +30,12 @@ def test_fuel_spent_to_align_position_part2_b(mock_get_crab_positions):
     mock_get_crab_positions.return_value = [0, 1, 1, 2, 2, 4, 5, 7, 14, 16]
     actual = fuel_spent_to_align_position_part2()
     expected = 162
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+def test_find_fuel_total():
+    actual = find_fuel_total([0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 2)
+    expected = 206
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
