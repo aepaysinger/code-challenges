83e1ca90a3be4f0b21e2e2572c67ddbd5a3d66de
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-06-13 10:55:12 -0500

    changed variable names

--- code_challenges/advent_of_code/hydrothermal_venture.py
+++ code_challenges/advent_of_code/hydrothermal_venture.py
@@ -20,7 +20,7 @@ def get_vents_input():
 def get_coordinate_pairs():
     vents_coordinates = get_vents_input()
     coordinate_pairs = []
-    
+
     biggest_x = 0
     biggest_y = 0
     j = 1
@@ -28,7 +28,12 @@ def get_coordinate_pairs():
     for coordinates in vents_coordinates:
         if j > len(vents_coordinates):
             break
-        coordinate_pairs.append([(int(vents_coordinates[i][0]), int(vents_coordinates[i][1])), (int(vents_coordinates[j][0]), int(vents_coordinates[j][1]))])
+        coordinate_pairs.append(
+            [
+                (int(vents_coordinates[i][0]), int(vents_coordinates[i][1])),
+                (int(vents_coordinates[j][0]), int(vents_coordinates[j][1])),
+            ]
+        )
         i += 2
         j += 2
 
@@ -45,35 +50,24 @@ def get_coordinate_pairs():
 def mark_coordinates_horizontal_vertical():
     coordinate_pairs, biggest_x, biggest_y = get_coordinate_pairs()
     coordinates_map = [[0 for i in range(biggest_x + 1)] for i in range(biggest_y + 1)]
-    j = 1
-    i = 0
-    for _ in range(len(coordinate_pairs)):
-        if j > len(coordinate_pairs):
-            break
-        if coordinate_pairs[i][1] == coordinate_pairs[j][1]:
-            if coordinate_pairs[i][0] < coordinate_pairs[j][0]:
-                for coordinate in range(
-                    coordinate_pairs[i][0], coordinate_pairs[j][0] + 1
-                ):
-                    coordinates_map[coordinate_pairs[i][1]][coordinate] += 1
+    x = 0
+    y = 1
+
+    for coordinates in coordinate_pairs:
+        if coordinates[0][y] == coordinates[1][y]:
+            if coordinates[0][x] < coordinates[1][x]:
+                for coordinate in range(coordinates[0][x], coordinates[1][x] + 1):
+                    coordinates_map[coordinates[0][y]][coordinate] += 1
             else:
-                for coordinate in range(
-                    coordinate_pairs[j][0], coordinate_pairs[i][0] + 1
-                ):
-                    coordinates_map[coordinate_pairs[i][1]][coordinate] += 1
-        if coordinate_pairs[i][0] == coordinate_pairs[j][0]:
-            if coordinate_pairs[j][1] < coordinate_pairs[i][1]:
-                for coordinate in range(
-                    coordinate_pairs[j][1], coordinate_pairs[i][1] + 1
-                ):
-                    coordinates_map[coordinate][coordinate_pairs[i][0]] += 1
+                for coordinate in range(coordinates[1][x], coordinates[0][x] + 1):
+                    coordinates_map[coordinates[0][y]][coordinate] += 1
+        elif coordinates[0][x] == coordinates[1][x]:
+            if coordinates[1][y] < coordinates[0][y]:
+                for coordinate in range(coordinates[1][y], coordinates[0][y] + 1):
+                    coordinates_map[coordinate][coordinates[0][x]] += 1
             else:
-                for coordinate in range(
-                    coordinate_pairs[i][1], coordinate_pairs[j][1] + 1
-                ):
-                    coordinates_map[coordinate][coordinate_pairs[i][0]] += 1
-        i += 2
-        j += 2
+                for coordinate in range(coordinates[0][y], coordinates[1][y] + 1):
+                    coordinates_map[coordinate][coordinates[0][x]] += 1
 
     danger_zone = 0
     for coordinates in coordinates_map:
@@ -86,68 +80,55 @@ def mark_coordinates_horizontal_vertical():
 def mark_coordinates_horizontal_vertical_diagonal():
     coordinate_pairs, biggest_x, biggest_y = get_coordinate_pairs()
     coordinates_map = [[0 for i in range(biggest_x + 1)] for i in range(biggest_y + 1)]
-
-    i = 0
-    j = 1
-    for _ in range(len(coordinate_pairs)):
-        if j > len(coordinate_pairs):
-            break
-        if coordinate_pairs[i][1] == coordinate_pairs[j][1]:
-            if coordinate_pairs[i][0] < coordinate_pairs[j][0]:
-                for coordinate in range(
-                    coordinate_pairs[i][0], coordinate_pairs[j][0] + 1
-                ):
-                    coordinates_map[coordinate_pairs[i][1]][coordinate] += 1
+    x = 0
+    y = 1
+    for coordinates in coordinate_pairs:
+        if coordinates[0][y] == coordinates[1][y]:
+            if coordinates[0][x] < coordinates[1][x]:
+                for coordinate in range(coordinates[0][x], coordinates[1][x] + 1):
+                    coordinates_map[coordinates[0][y]][coordinate] += 1
             else:
-                for coordinate in range(
-                    coordinate_pairs[j][0], coordinate_pairs[i][0] + 1
-                ):
-                    coordinates_map[coordinate_pairs[i][1]][coordinate] += 1
-        elif coordinate_pairs[i][0] == coordinate_pairs[j][0]:
-            if coordinate_pairs[j][1] < coordinate_pairs[i][1]:
-                for coordinate in range(
-                    coordinate_pairs[j][1], coordinate_pairs[i][1] + 1
-                ):
-                    coordinates_map[coordinate][coordinate_pairs[i][0]] += 1
+                for coordinate in range(coordinates[1][x], coordinates[0][x] + 1):
+                    coordinates_map[coordinates[0][y]][coordinate] += 1
+        elif coordinates[0][x] == coordinates[1][x]:
+            if coordinates[1][y] < coordinates[0][y]:
+                for coordinate in range(coordinates[1][y], coordinates[0][y] + 1):
+                    coordinates_map[coordinate][coordinates[0][x]] += 1
             else:
-                for coordinate in range(
-                    coordinate_pairs[i][1], coordinate_pairs[j][1] + 1
-                ):
-                    coordinates_map[coordinate][coordinate_pairs[i][0]] += 1
+                for coordinate in range(coordinates[0][y], coordinates[1][y] + 1):
+                    coordinates_map[coordinate][coordinates[1][x]] += 1
         elif (
-            coordinate_pairs[j][0] < coordinate_pairs[i][0]
-            and coordinate_pairs[j][1] < coordinate_pairs[i][1]
+            coordinates[1][x] < coordinates[0][x]
+            and coordinates[1][y] < coordinates[0][y]
         ):
-            x_coordinate = coordinate_pairs[j][0]
-            for coordinate in range(coordinate_pairs[j][1], coordinate_pairs[i][1] + 1):
+            x_coordinate = coordinates[1][x]
+            for coordinate in range(coordinates[1][y], coordinates[0][y] + 1):
                 coordinates_map[coordinate][x_coordinate] += 1
                 x_coordinate += 1
         elif (
-            coordinate_pairs[i][0] < coordinate_pairs[j][0]
-            and coordinate_pairs[i][1] > coordinate_pairs[j][1]
+            coordinates[0][x] < coordinates[1][x]
+            and coordinates[0][y] > coordinates[1][y]
         ):
-            x_coordinate = coordinate_pairs[j][0]
-            for coordinate in range(coordinate_pairs[j][1], coordinate_pairs[i][1] + 1):
+            x_coordinate = coordinates[1][x]
+            for coordinate in range(coordinates[1][y], coordinates[0][y] + 1):
                 coordinates_map[coordinate][x_coordinate] += 1
                 x_coordinate -= 1
         elif (
-            coordinate_pairs[i][0] < coordinate_pairs[j][0]
-            and coordinate_pairs[i][1] < coordinate_pairs[j][1]
+            coordinates[0][x] < coordinates[1][x]
+            and coordinates[0][y] < coordinates[1][y]
         ):
-            x_coordinate = coordinate_pairs[i][0]
-            for coordinate in range(coordinate_pairs[i][0], coordinate_pairs[j][0] + 1):
+            x_coordinate = coordinates[0][x]
+            for coordinate in range(coordinates[0][x], coordinates[1][x] + 1):
                 coordinates_map[coordinate][coordinate] += 1
         elif (
-            coordinate_pairs[j][0] < coordinate_pairs[i][0]
-            and coordinate_pairs[j][1] > coordinate_pairs[i][1]
+            coordinates[1][x] < coordinates[0][x]
+            and coordinates[1][y] > coordinates[0][y]
         ):
-            y_coordinate = coordinate_pairs[j][1]
-            for coordinate in range(coordinate_pairs[j][0], coordinate_pairs[i][0] + 1):
+            y_coordinate = coordinates[1][y]
+            for coordinate in range(coordinates[1][x], coordinates[0][x] + 1):
                 coordinates_map[coordinate][y_coordinate] += 1
                 y_coordinate -= 1
 
-        i += 2
-        j += 2
     danger_zone = 0
     for coordinates in coordinates_map:
         for coordinate in coordinates:
@@ -159,6 +140,6 @@ def mark_coordinates_horizontal_vertical_diagonal():
 
 # print(get_vents_input())
 # print(mark_coordinates_horizontal_vertical())
-# print(mark_coordinates_horizontal_vertical_diagonal())
-print(get_coordinate_pairs())
+print(mark_coordinates_horizontal_vertical_diagonal())
+# print(get_coordinate_pairs())
 # 19424 too lovw
