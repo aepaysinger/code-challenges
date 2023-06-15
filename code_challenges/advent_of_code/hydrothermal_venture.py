b0d99fc1df51cc7b7c360f45f49bbb0b5bb36f97
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-06-15 13:02:55 -0500

    cleaned up and rewrote tests

--- code_challenges/advent_of_code/hydrothermal_venture.py
+++ code_challenges/advent_of_code/hydrothermal_venture.py
@@ -2,6 +2,11 @@ def get_vents_input():
     with open("code_challenges/advent_of_code/vents_input") as vents_input:
         vents_coordinates = vents_input.read().split("\n")
 
+    return vents_coordinates
+
+
+def transform_vents_input():
+    vents_coordinates = get_vents_input()
     vents_coordinates = [
         vent_coordinate.split("->") for vent_coordinate in vents_coordinates
     ]
@@ -16,8 +21,9 @@ def get_vents_input():
 
     return vents_coordinates
 
+
 def get_coordinate_pairs():
-    vents_coordinates = get_vents_input()
+    vents_coordinates = transform_vents_input()
     coordinate_pairs = []
 
     j = 1
@@ -37,7 +43,7 @@ def get_coordinate_pairs():
 
     return coordinate_pairs
 
-def count_coordinate_point():
+def count_coordinate_points():
     coordinate_pairs = get_coordinate_pairs()
     coordinate_points = {}
     count = 0
@@ -64,7 +70,7 @@ def count_coordinate_point():
         elif x1 < x2 and y1 > y2:
             x_coordinate = x2
             for coordinate in range(y2, y1 + 1):
-                coordinate_points[(x_coordinate, coordinate)] = coordinate_points.get((coordinate, x_coordinate), 0) + 1
+                coordinate_points[(x_coordinate, coordinate)] = coordinate_points.get((x_coordinate, coordinate), 0) + 1
                 x_coordinate -= 1
         elif x1 > x2 and y1 < y2:
             y_coordinate = y2
@@ -82,105 +88,14 @@ def count_coordinate_point():
         if amount >= 2:
             count += 1
         
-    return coordinate_points, count
-
-
-def mark_coordinates_horizontal_vertical():
-    coordinate_pairs, biggest_x, biggest_y = get_coordinate_pairs()
-    coordinates_map = [[0 for i in range(biggest_x + 1)] for i in range(biggest_y + 1)]
-    print(coordinates_map)
-    x = 0
-    y = 1
-
-    for (x1, y1), (x2, y2) in coordinate_pairs:
-        if y1 == y2:
-            if x1 < x2:
-                for coordinate in range(x1, x2 + 1):
-                    coordinates_map[y1][coordinate] += 1
-            else:
-                for coordinate in range(x2, x1 + 1):
-                    coordinates_map[y1][coordinate] += 1
-        elif x1 == x2:
-            if y2 < y1:
-                for coordinate in range(y2, y1 + 1):
-                    coordinates_map[coordinate][x1] += 1
-            else:
-                for coordinate in range(y1, y2 + 1):
-                    coordinates_map[coordinate][x1] += 1
-
-    danger_zone = 0
-    for coordinates in coordinates_map:
-        for coordinate in coordinates:
-            if coordinate >= 2:
-                danger_zone += 1
-    return danger_zone
-
-
-# def mark_coordinates_horizontal_vertical_diagonal():
-#     coordinate_pairs, biggest_x, biggest_y = get_coordinate_pairs()
-#     coordinates_map = [[0 for i in range(biggest_x + 1)] for i in range(biggest_y + 1)]
- 
-#     for (x1, y1), (x2, y2) in coordinate_pairs:
-#         # if y1 == y2:
-#         #     if x1 < x2:
-#         #         for coordinate in range(x1, x2 + 1):
-#         #             coordinates_map[y1][coordinate] += 1
-#         #     else:
-#         #         for coordinate in range(x2, x1 + 1):
-#         #             coordinates_map[y1][coordinate] += 1
-#         # elif x1 == x2:
-#         #     if y2 < y1:
-#         #         for coordinate in range(y2, y1 + 1):
-#         #             coordinates_map[coordinate][x1] += 1
-#         #     else:
-#         #         for coordinate in range(y1, y2 + 1):
-#         #             coordinates_map[coordinate][x1] += 1
-#         elif x2 < x1 and y2 < y1:
-#             x_coordinate = x2
-#             for coordinate in range(y2, y1 + 1):
-#                 coordinates_map[coordinate][x_coordinate] += 1
-#                 x_coordinate += 1
-#         elif x1 < x2 and y1 > y2:
-#             x_coordinate = x2
-#             for coordinate in range(y2, y1 + 1):
-#                 coordinates_map[coordinate][x_coordinate] += 1
-#                 x_coordinate -= 1
-#         elif x1 < x2 and y1 < y2:
-#             x_coordinate = x1
-#             for coordinate in range(x1, x2 + 1):
-#                 coordinates_map[coordinate][coordinate] += 1
-#         elif x2 < x1 and y2 > y1:
-#             y_coordinate = y2
-#             for coordinate in range(x2, x1 + 1):
-#                 coordinates_map[coordinate][y_coordinate] += 1
-#                 y_coordinate -= 1
-
-#     danger_zone = 0
-#     for coordinates in coordinates_map:
-#         for coordinate in coordinates:
-#             if coordinate >= 2:
-#                 danger_zone += 1
-
-#     return danger_zone
+    return count
 
 
 # print(get_vents_input())
-# print(mark_coordinates_horizontal_vertical())
-# print(mark_coordinates_horizontal_vertical_diagonal())
-# print(get_coordinate_pairs())
-# 19424 too lovw
-print(count_coordinate_point())
-# [[1, 0, 1, 0, 0, 0, 0, 1, 1, 0], 
-#  [0, 1, 1, 1, 0, 0, 0, 2, 0, 0], 
-#  [0, 0, 2, 0, 1, 0, 1, 1, x, 0], 
-#  [0, 0, 0, 1, 0, 2, 0, X, 0, 0], 
-#  [0, 1, 1, 2, 3, 1, x, 2, 1, 1], 
-#  [0, 0, 0, 1, 0, x, 0, 0, 0, 0], 
-#  [0, 0, 1, 0, 0, 0, 1, 0, 0, 0], 
-#  [0, 1, 0, 0, 0, 0, 0, 1, 0, 0], 
-#  [1, 0, 0, 0, 0, 0, 0, 0, 1, 0], 
-#  [2, 2, 2, 1, 1, 1, 0, 0, 0, 0]]
+# print(transform_vents_input())
+print(get_coordinate_pairs())
+# print(count_coordinate_points())
 
-# 5,5 -> 8,2
 
-# 7,0 -> 7,4
+# 19424 too low
+# 17107 too low
--- tests/advent_of_code/test_hydrothermal_venture.py
+++ tests/advent_of_code/test_hydrothermal_venture.py
@@ -1,97 +1,31 @@
 from unittest.mock import patch
 
-from code_challenges.advent_of_code.hydrothermal_venture import (
-    mark_coordinates_horizontal_vertical,
-    mark_coordinates_horizontal_vertical_diagonal,
-    get_coordinate_pairs,
-)
+from code_challenges.advent_of_code.hydrothermal_venture import transform_vents_input, get_coordinate_pairs, count_coordinate_points
 
 
 @patch("code_challenges.advent_of_code.hydrothermal_venture.get_vents_input")
-def test_mark_coordinates_horizontal_vertical(mock_get_vents_input):
-    mock_get_vents_input.return_value = [
-        ["0", "9"],
-        ["5", "9"],
-        ["8", "0"],
-        ["0", "8"],
-        ["9", "4"],
-        ["3", "4"],
-        ["2", "2"],
-        ["2", "1"],
-        ["7", "0"],
-        ["7", "4"],
-        ["6", "4"],
-        ["2", "0"],
-        ["0", "9"],
-        ["2", "9"],
-        ["3", "4"],
-        ["1", "4"],
-        ["0", "0"],
-        ["8", "8"],
-        ["5", "5"],
-        ["8", "2"],
-    ]
-    actual = mark_coordinates_horizontal_vertical()
-    expected = 5
-
-    assert actual == expected, f"Returned {actual} instead of {expected}"
-
-
-@patch("code_challenges.advent_of_code.hydrothermal_venture.get_vents_input")
-def test_mark_coordinates_horizontal_vertical_diagonal(mock_get_vents_input):
-    mock_get_vents_input.return_value = [
-        ["0", "9"],
-        ["5", "9"],
-        ["8", "0"],
-        ["0", "8"],
-        ["9", "4"],
-        ["3", "4"],
-        ["2", "2"],
-        ["2", "1"],
-        ["7", "0"],
-        ["7", "4"],
-        ["6", "4"],
-        ["2", "0"],
-        ["0", "9"],
-        ["2", "9"],
-        ["3", "4"],
-        ["1", "4"],
-        ["0", "0"],
-        ["8", "8"],
-        ["5", "5"],
-        ["8", "2"],
-    ]
-    actual = mark_coordinates_horizontal_vertical_diagonal()
-    expected = 12
+def test_transform_vents_input(mock_get_vents_input):
+    mock_get_vents_input.return_value = ['0,9 -> 5,9', '8,0 -> 0,8', '9,4 -> 3,4', '2,2 -> 2,1', '7,0 -> 7,4', '6,4 -> 2,0', '0,9 -> 2,9', '3,4 -> 1,4', '0,0 -> 8,8', '5,5 -> 8,2']
+    actual = transform_vents_input()
+    expected = [['0', '9'], ['5', '9'], ['8', '0'], ['0', '8'], ['9', '4'], ['3', '4'], ['2', '2'], ['2', '1'], ['7', '0'], ['7', '4'], ['6', '4'], ['2', '0'], ['0', '9'], ['2', '9'], ['3', '4'], ['1', '4'], ['0', '0'], ['8', '8'], ['5', '5'], ['8', '2']]
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
 
 
 @patch("code_challenges.advent_of_code.hydrothermal_venture.get_vents_input")
 def test_get_coordinate_pairs(mock_get_vents_input):
-    mock_get_vents_input.return_value = [
-        ["0", "9"],
-        ["5", "9"],
-        ["8", "0"],
-        ["0", "8"],
-        ["9", "4"],
-        ["3", "4"],
-        ["2", "2"],
-        ["2", "1"],
-        ["7", "0"],
-        ["7", "4"],
-        ["6", "4"],
-        ["2", "0"],
-        ["0", "9"],
-        ["2", "9"],
-        ["3", "4"],
-        ["1", "4"],
-        ["0", "0"],
-        ["8", "8"],
-        ["5", "5"],
-        ["8", "2"],
-    ]
+    mock_get_vents_input.return_value = ['0,9 -> 5,9', '8,0 -> 0,8', '9,4 -> 3,4', '2,2 -> 2,1', '7,0 -> 7,4', '6,4 -> 2,0', '0,9 -> 2,9', '3,4 -> 1,4', '0,0 -> 8,8', '5,5 -> 8,2']
     actual = get_coordinate_pairs()
-    expected = ([[(0, 9), (5, 9)], [(8, 0), (0, 8)], [(9, 4), (3, 4)], [(2, 2), (2, 1)], [(7, 0), (7, 4)], [(6, 4), (2, 0)], [(0, 9), (2, 9)], [(3, 4), (1, 4)], [(0, 0), (8, 8)], [(5, 5), (8, 2)]], 9, 9)
+    expected = [[(0, 9), (5, 9)], [(8, 0), (0, 8)], [(9, 4), (3, 4)], [(2, 2), (2, 1)], [(7, 0), (7, 4)], [(6, 4), (2, 0)], [(0, 9), (2, 9)], [(3, 4), (1, 4)], [(0, 0), (8, 8)], [(5, 5), (8, 2)]]
 
     assert actual == expected, f"Returned {actual} instead of {expected}"
+
+
+@patch("code_challenges.advent_of_code.hydrothermal_venture.get_vents_input")
+def test_count_coordinate_points(mock_get_vents_input):
+    mock_get_vents_input.return_value = ['0,9 -> 5,9', '8,0 -> 0,8', '9,4 -> 3,4', '2,2 -> 2,1', '7,0 -> 7,4', '6,4 -> 2,0', '0,9 -> 2,9', '3,4 -> 1,4', '0,0 -> 8,8', '5,5 -> 8,2']
+    actual = count_coordinate_points()
+    expected = 12
+
+    assert actual == expected, f"Returned {actual} instead of {expected}"
+
