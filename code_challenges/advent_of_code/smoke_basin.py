757a17accb59ea6ce16b0af279353cf020c51b45
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-06-21 10:57:43 -0500

    advent smoke basin

--- code_challenges/advent_of_code/smoke_basin.py
+++ code_challenges/advent_of_code/smoke_basin.py
@@ -7,15 +7,31 @@ def find_lowest_point():
     smoke_levels = get_smoke_input()
     # print(smoke_levels)
     # smoke_levels = [row.split() for row in smoke_levels]
-    
-    lowest_points = []
-    for i, level in enumerate(smoke_levels):
-        behind = i - 1
-        before = i + 1
-        above = smoke_levels[i - 1][i]
-        under = smoke_levels[i + 1][i]
-        if i == 0:
-            print(level)
+    # smoke_levels = [[','.join(number)]for row in smoke_levels for number in row]
+    # smoke_levels = []
+    print(smoke_levels)
+    # res = [int(test_str[idx : idx + K]) for idx in range(0, len(test_str), K)]
+    # lowest_points = []
+    # for i, level in enumerate(smoke_levels):
+    #     if i >= 1 and i <= 3:
+    #         behind = i - 1
+    #         after = i + 1
+    #         above = smoke_levels[i - 1][i]
+    #         under = smoke_levels[i + 1][i]
+    #         for i, number in enumerate(level):
+    #             number = int(number)
+    #             if i == 0:
+    #                 if number < level[above] and number < level[after] and number < level[under]:
+    #                     lowest_points.append(number)
+    #             elif i == len(level) - 1:
+    #                 if number < level[above] and number < level[behind] and number < level[under]:
+    #                     lowest_points.append(number)
+    #             else:
+    #                 if number < level[behind] and number < level[after] and number < level[above] and number < level[under]:
+    #                     lowest_points.append(number)
+    # print(lowest_points)
+                
+                
 
         
     
