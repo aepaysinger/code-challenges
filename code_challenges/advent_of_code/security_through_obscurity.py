99563a4a6c1c4b6d0bd80f084961b1bac42fa74d
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-07-10 08:46:38 -0500

    advent security through obscurity

---
+++ code_challenges/advent_of_code/security_through_obscurity.py
@@ -0,0 +1,23 @@
+def get_room_names():
+    with open("./code_challenges/advent_of_code/room_name_input") as room_names_input:
+        room_names = room_names_input.read().split("\n")
+    return room_names
+
+def get_incrypted_room_name():
+    room_names = get_room_names()
+    room_names = [room.split("-") for room in room_names]
+    for room in room_names:
+        letter_count = {}
+        for name in range(len(room)-1):
+            for character in name:
+                print(character)
+                # letter_count[character] = letter_count.get(character, 0) + 1
+        # print(letter_count)
+
+    
+
+    return room_names
+
+
+# print(get_room_names())
+print(get_incrypted_room_name())
