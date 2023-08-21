8da612bb4d9a3bc5964f984a4f96d3de0b972305
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-08-21 12:45:20 -0500

    figuring out last part

--- code_challenges/advent_of_code/remove_opposite_polarities.py
+++ code_challenges/advent_of_code/remove_opposite_polarities.py
@@ -41,35 +41,41 @@ def check_two_letters(letter_one, letter_two):
 
 def find_polymer_characters():
     polymers = get_polymers()
-    polymer_counts = {}
+    polymer_characters = set()
     for character in polymers:
-        polymer_counts[f"{character.upper()}/{character.lower()}"] = polymer_counts.get(f"{character.upper()}/{character.lower()}", 0)
-    return polymer_counts
+        character = character.upper()
+        polymer_characters.add(character)
+    return polymer_characters
 
 
 def find_polymer_counts():
     polymers = get_polymers()
-    print(polymers)
-    
-    polymer_count = find_polymer_characters()
-    print(polymer_count)
-    for big,_,little in polymer_count.keys():
-        edited_polymers = []
-        for character in polymers:
-            if character == big:
-                continue
-            elif character == little:
-                continue
-            else:
-                edited_polymers.append(character)
-        edited_polymers = find_polymers_with_no_reactions(edited_polymers)
-        polymer_count[big,_,little] = len(edited_polymers)
-        polymers = get_polymers()
+    # print(polymers)
+    polymer_chracters = find_polymer_characters()
+    reactions_removing_characters = {}
+    # while len(polymar_chracters) > 0:
+    # print(reactions_removing_characters)
+    for character in polymer_chracters:
+        print(character)
+        changed_polymers = get_polymers()
+        for item in changed_polymers:
+            if item == character.upper() or item == character.lower():
+                changed_polymers.remove(item)
+        print(changed_polymers)
+        reactions_removing_characters[character] = len(find_polymers_with_no_reactions(changed_polymers))
+        # print(reactions_removing_characters)
+        
+        # print(changed_polymers)
+    # return reactions_removing_characters
+        
+
+
+
         
         # edited_polymers = find_polymers_with_no_reactions(polymers)
         # polymer_count[big,_,little] = len(edited_polymers)
         
-    print(polymer_count)
+    # print(polymer_count)
 
             
 
