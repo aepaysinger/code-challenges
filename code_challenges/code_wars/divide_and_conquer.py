63af6d5272b0d87cd1317b5dd2cdb786c9cbf771
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-08-28 08:47:34 -0500

    CW divide and conquer

---
+++ code_challenges/code_wars/divide_and_conquer.py
@@ -0,0 +1,13 @@
+def consecutive_nums(lst, group_len):
+    pass
+
+def sort_groups():
+    
+
+
+
+if __name__ == "__main__":
+    lst = [1, 2, 3, 6, 2, 3, 4, 7, 8]
+    group_len = 3
+    # print(consecutive_nums(lst, group_len))
+    print(sort_groups(lst, group_len))
