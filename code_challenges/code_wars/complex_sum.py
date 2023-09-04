36ba9f6205ed174eb0782d3f009bc4057bd52a32
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-04 11:54:01 -0500

    cw complex sum

---
+++ code_challenges/code_wars/complex_sum.py
@@ -0,0 +1,11 @@
+def complex_sum(arr):
+    real_nums = []
+    imaginary_nums = []
+    for nums in arr:
+        print(nums)
+    
+
+
+if __name__ == "__main__":
+    arr = ['2+3i', '3-i']
+    print(complex_sum(arr))
