88ed22aa7ed66c4c31a7a793ad077f1a1fbb4467
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-10 15:09:17 -0500

    changed function

--- code_challenges/code_wars/two_sum.py
+++ code_challenges/code_wars/two_sum.py
@@ -1,5 +1,6 @@
 def two_sum(numbers, target):
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == target:
-                return [i, j]
+    seen = {}
+    for i, num in enumerate(numbers):
+        if target - num in seen:
+            return [seen[target - num], i]
+        seen[num] = i
