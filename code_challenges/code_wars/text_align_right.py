3f66e6550842d1609003e8de83138af0d3520f33
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-18 08:00:38 -0500

    CW text align right

---
+++ code_challenges/code_wars/text_align_right.py
@@ -0,0 +1,18 @@
+import re
+
+
+def align_right(text,width):
+    # aligned_text = ""
+    # for words in text:
+        
+
+    #     print(words)
+    print(re.split(r'(\W+,)', text))
+
+
+
+
+if __name__ == "__main__":
+    text = "Two lines, I am"
+    width = 10
+    print(align_right(text, width))
