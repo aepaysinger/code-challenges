7685bb307253652f039ecf0f1a88f1c0775fe811
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-09-28 11:00:43 -0500

    cw find the time complete

--- code_challenges/code_wars/find_the_time.py
+++ code_challenges/code_wars/find_the_time.py
@@ -1,18 +1,10 @@
 def what_time_is_it(angle):
-    hour = int(angle//30)
+    hour = int(angle // 30)
     min = int((angle - (hour * 30)) * 2)
     if min < 10:
         min = "0" + str(min)
-    if min == 0:
-        min = "00"
     if hour < 10:
         hour = "0" + str(hour)
     if hour == "00":
         hour = 12
     return f"{hour}:{min}"
-
-
-
-if __name__ == "__main__":
-    angle = 193.266
-    print(what_time_is_it(angle))
