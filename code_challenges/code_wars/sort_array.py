b34516bcc03e7dcf22894a28142b7e518783fa9b
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-10-24 08:08:01 -0500

    tetris

--- code_challenges/code_wars/sort_array.py
+++
@@ -1,8 +0,0 @@
-def sort_array(numbers_to_sort):
-    odds = iter(sorted(number for number in numbers_to_sort if number % 2))
-    return [next(odds) if number % 2 else number for number in numbers_to_sort]
-
-
-if __name__ == "__main__":
-    numbers_to_sort = [5, 8, 6, 3, 4]
-    print(sort_array(numbers_to_sort)) 
