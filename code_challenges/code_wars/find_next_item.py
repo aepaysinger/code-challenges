2324dfe09b621d4334fbc844eac165d42ffc5a3d
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-10-27 12:40:21 -0500

    ran black

--- code_challenges/code_wars/find_next_item.py
+++ code_challenges/code_wars/find_next_item.py
@@ -1,4 +1,3 @@
-
 def cycle(direction, values, value):
     for i in range(len(values)):
         if values[i] == value:
@@ -6,7 +5,6 @@ def cycle(direction, values, value):
                 return values[0]
             if i == 0 and direction < 0:
                 return values[-1]
-            
+
             return values[i + direction]
     return None
-
