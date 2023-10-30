c3daf4c97385ea2d27c6773eb8d098e61af591d9
Amelia Paysinger <ameliapaysinger@gmail.com>
2023-10-30 09:38:54 -0500

    removed extra (), added list comprehension

--- code_challenges/code_wars/almost_even.py
+++ code_challenges/code_wars/almost_even.py
@@ -1,10 +1,8 @@
 def split_integer(num, parts):
-    integer_parts = []
     part = num // parts
-    for _ in range(parts):
-        integer_parts.append(part)
+    integer_parts = [part for _ in range(parts)]
     i = len(integer_parts) - 1
-    for _ in range((num % parts)):
+    for _ in range(num % parts):
         integer_parts[i] += 1
         i -= 1
     return integer_parts
